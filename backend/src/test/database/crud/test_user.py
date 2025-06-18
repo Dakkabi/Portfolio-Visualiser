f0593b17e502c87510af8be04756508262d7d675
Dominic <DominicArda@icloud.com>
2025-06-18 16:37:31 +0100

    #7 Create test case for crud_user

---
+++ backend/src/test/database/crud/test_user.py
@@ -0,0 +1,20 @@
+import pytest
+from sqlalchemy.orm.session import Session
+from database.crud.user import *
+from database.session import get_db
+from schemas.user import UserCreate
+
+
+@pytest.fixture
+def db() -> Session:
+    return get_db()
+
+def test_create_and_get_user(db):
+    user = UserCreate(
+        email="test@test.com",
+        password="plaintext123"
+    )
+
+    create_user(next(db), user)
+
+
