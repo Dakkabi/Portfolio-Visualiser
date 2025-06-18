611c206b9b13e45d0570500864610ed7ce30d1a6
Dominic <DominicArda@icloud.com>
2025-06-18 18:00:46 +0100

    #7 Create test case for db generation, pytest method db Session generator with rollback

---
+++ backend/src/test/database/test_session.py
@@ -0,0 +1,26 @@
+from backend.src.database.session import get_db, Base
+import pytest
+import os
+from sqlalchemy.orm.session import Session
+from alembic import command
+from alembic.config import Config
+
+# What the hell.
+ALEMBIC_INI_PATH = os.path.join(os.path.abspath(__file__), "..", "..", "..", "..", "..", "alembic.ini")
+
+@pytest.fixture
+def db() -> Session:
+    db_session = next(get_db())
+
+    alembic_cfg = Config(ALEMBIC_INI_PATH)
+    command.upgrade(alembic_cfg, "head")
+
+    db_session.begin() # Everything after here will be rolled back
+
+    yield db_session
+
+    db_session.rollback()
+    db_session.close()
+
+def test_db(db):
+    assert db is not None
