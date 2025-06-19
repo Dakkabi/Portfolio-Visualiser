b5a18e12dfbf7a1772b9bb04ddd1d67c160fe492
Dominic <DominicArda@icloud.com>
2025-06-19 16:00:11 +0100

    #13 Create auth services for handling JWT tokens with methods for creating tokens, authenticating user and getting user from token

---
+++ backend/src/services/auth_service.py
@@ -0,0 +1,96 @@
+from datetime import timedelta, timezone, datetime
+import jwt
+from fastapi import HTTPException, status
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+from sqlalchemy.orm import Session
+from typing import Annotated, Any, Coroutine
+from backend.src.core.config import settings
+from backend.src.core.security import verify_password
+from backend.src.database.crud.user_crud import get_user_by_email
+from backend.src.database.models.user_model import User
+from backend.src.database.session import get_db
+from backend.src.schemas.token_schema import TokenData
+
+ALGORITHM = "HS256"
+JWT_SECRET_KEY = settings.JWT_SECRET_KEY
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+
+def authenticate_user(email: str, password: str, db: Session = Depends(get_db)) -> bool | type[User]:
+    """
+    Authenticate a user by email and password.
+
+    :param email: The email of the user.
+    :param password: The password inputted.
+    :param db: The database connection.
+    :return: User Schema if the user is authenticated, False otherwise.
+    """
+
+    user = get_user_by_email(db, email)
+    if not user:
+        return False
+
+    if not verify_password(password, str(user.password)):
+        return False
+
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Return a dict signed with a JWT token.
+
+    :param data: The data to encode.
+    :param expires_delta: The token expiration time in seconds.
+    :return: A dict signed with the JWT token.
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_scheme)],
+        db: Session = Depends(get_db)
+    ) -> type[User]:
+    """
+    Get the user from the email from the token.
+
+    :param token: The signed token.
+    :param db: The database connection.
+    :return: User Schema if the user is authenticated, False otherwise.
+    :raises: HTTPException if no email exists, invalid token or no user exists.
+    """
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+
+        token_data = TokenData(email=email)
+
+    except InvalidTokenError:
+        raise credentials_exception
+
+    user = get_user_by_email(db, email=token_data.email)
+    if user is None:
+        raise credentials_exception
+
+    return user
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    """Get user from the token, handled by FastAPI Dependency Injection."""
+    return current_user
