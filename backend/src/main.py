38031095d55c011ff1a7b9178a24c6fbee82026e
Dominic <DominicArda@icloud.com>
2025-06-19 16:09:13 +0100

    #13 Include auth_router endpoints in entrypoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
+from backend.src.routes.auth_router import auth_router
+
 app = FastAPI()
+
+app.include_router(auth_router, prefix="/api", tags=["auth"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
