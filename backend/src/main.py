09254208c9d729cd3ab7c6f88f2f27890031ee1c
Dominic <DominicArda@icloud.com>
2025-06-19 18:24:07 +0100

    #13 make tags handled by the router class

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,8 +5,8 @@ from backend.src.routes.models.user_router import user_router
 
 app = FastAPI()
 
-app.include_router(auth_router, prefix="/api", tags=["auth"])
-app.include_router(user_router, prefix="/api", tags=["users"])
+app.include_router(auth_router, prefix="/api")
+app.include_router(user_router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
