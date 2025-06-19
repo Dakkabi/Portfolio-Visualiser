232f84c55b6db6be3561856c7833148666bfafce
Dominic <DominicArda@icloud.com>
2025-06-19 18:24:20 +0100

    #13 Add get current user from token method as endpoint

--- backend/src/routes/models/user_router.py
+++ backend/src/routes/models/user_router.py
@@ -6,11 +6,16 @@ from sqlalchemy.orm import Session
 from backend.src.database.crud.user_crud import *
 from backend.src.database.session import get_db
 from backend.src.schemas.model.user_schema import UserSchema
+from backend.src.services.auth.auth_service import get_current_active_user
 
 user_router = APIRouter(
     prefix="/users",
     tags=["users"]
 )
+
+@user_router.get("/whoami", response_model=UserSchema)
+def user_whoami(current_user: User = Depends(get_current_active_user)):
+    return current_user
 
 @user_router.get("/", response_model=List[UserSchema])
 def get_all_users(db: Session = Depends(get_db)):
@@ -25,7 +30,7 @@ def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 @user_router.post("/", response_model=UserSchema)
-def user_post(user: UserCreate, db: Session = Depends(get_db)):
+def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db, user)
 
 
