1f177c4aae21f462ebe1fac3396b2d5c0d419fdd
Dominic <DominicArda@icloud.com>
2025-06-19 16:07:10 +0100

    #13 Create fastAPI endpoint for creating an access token on successful login

--- backend/src/routes/auth_router.py
+++ backend/src/routes/auth_router.py
@@ -1,6 +1,33 @@
-from fastapi import APIRouter
+from datetime import timedelta
+from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Annotated
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from backend.src.database.session import get_db
+from backend.src.schemas.token_schema import Token
+from backend.src.services.auth_service import authenticate_user, create_access_token
 
-auth_router = APIRouter(
+router = APIRouter(
     prefix="/auth",
     tags=["auth"]
 )
+
+@router.post('/login')
+async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        db: Session = Depends(get_db)
+) -> Token:
+    user = authenticate_user(form_data.email, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    access_token_expires = timedelta(minutes=1440)
+    access_token = create_access_token(
+        data={"sub": user.email},
+        expires_delta=access_token_expires
+    )
+
+    return Token(access_token=access_token, token_type="bearer")
