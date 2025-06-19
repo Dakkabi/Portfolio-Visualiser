cbe40c5a48f018db7ead192fc98e3aa5cac57c0b
Dominic <DominicArda@icloud.com>
2025-06-19 17:45:59 +0100

    #15 Formatting and corrections

--- backend/src/routes/auth/auth_router.py
+++ backend/src/routes/auth/auth_router.py
@@ -8,26 +8,25 @@ from backend.src.schemas.auth.token_schema import Token
 from backend.src.services.auth.auth_service import authenticate_user, create_access_token
 
 auth_router = APIRouter(
-    prefix="/auth",
-    tags=["auth"]
+    prefix='/auth',
+    tags=['Auth'],
 )
 
 @auth_router.post('/login')
 async def login_for_access_token(
-        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-        db: Session = Depends(get_db)
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Session = Depends(get_db)
 ) -> Token:
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"}
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=1440)
     access_token = create_access_token(
-        data={"sub": user.email},
-        expires_delta=access_token_expires
+        data={"sub": user.email}, expires_delta=access_token_expires
     )
 
     return Token(access_token=access_token, token_type="bearer")
