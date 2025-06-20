a9db5a5f4e4d2b98cbd7d4f1c72eaedfd49b77e2
Dominic <DominicArda@icloud.com>
2025-06-20 15:01:04 +0100

    #18 Create separation of production and test databases for unit testing

--- backend/test/database/test_session.py
+++ backend/test/database/test_session.py
@@ -1,28 +1,24 @@
+from sqlalchemy import create_engine
+from backend.src.core.config import settings
 from backend.src.database.session import get_db, Base
 import pytest
-import os
-from sqlalchemy.orm.session import Session
-from alembic import command
-from alembic.config import Config
+from sqlalchemy.orm.session import Session, sessionmaker
 
-# What the hell.
-ALEMBIC_INI_PATH = os.path.join(os.path.abspath(__file__), "..", "..", "..", "..", "alembic.ini")
 
 @pytest.fixture
 def db() -> Session:
-    db_session = next(get_db())
+    DATABASE_TEST_URI = settings.SQLALCHEMY_TEST_DATABASE_URI
+    engine = create_engine(str(DATABASE_TEST_URI))
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db_session = SessionLocal()
 
-    alembic_cfg = Config(ALEMBIC_INI_PATH)
-    command.upgrade(alembic_cfg, "head")
-
-    # TODO: Find a less destructive way to implement this
-    # Perhaps a separate Test Server?
-    Base.metadata.drop_all(bind=db_session.bind)
-    Base.metadata.create_all(bind=db_session.bind)
+    base = Base()
+    base.metadata.drop_all(bind=engine)
+    base.metadata.create_all(bind=engine)
 
     yield db_session
 
     db_session.close()
 
-def test_db(db):
-    assert db is not None
+def test_db(prod_db = next(get_db())):
+    assert prod_db is not None
