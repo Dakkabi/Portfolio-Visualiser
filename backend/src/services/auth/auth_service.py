349f1a3d554a45dc8847bc723721c364cd0eeabf
Dominic <DominicArda@icloud.com>
2025-06-20 17:02:08 +0100

    #18 Add decode JWT token method

--- backend/src/services/auth/auth_service.py
+++ backend/src/services/auth/auth_service.py
@@ -54,6 +54,19 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+def decode_access_token(token: str) -> dict:
+    """
+    Decode a JWT token.
+
+    There should be zero reason to use this function over get_current_method(...)
+    apart from debugging purposes.
+
+    :param token: The JWT token.
+    :return: The dict retrieved by decoding the JWT token.
+    """
+    return jwt.decode(token, JWT_SECRET_KEY, algorithms=ALGORITHM)
+
+
 async def get_current_user(
         token: Annotated[str, Depends(oauth2_scheme)],
         db: Session = Depends(get_db)
