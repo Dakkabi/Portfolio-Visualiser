b66689fa9b73e9891378c5b567db2555afdc63d0
Dominic <DominicArda@icloud.com>
2025-06-24 20:28:35 +0100

    #25 Add alert banners on whether the api key is valid or not

--- frontend/src/routes/settings/api/+page.svelte
+++ frontend/src/routes/settings/api/+page.svelte
@@ -73,6 +73,12 @@
 <input type="checkbox" id="trading212_modal" class="modal-toggle">
 <div class="modal" role="dialog">
     <div class="modal-box bg-base-200">
+        <div role="alert" class="alert alert-success">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>Your API Key has been accepted!</span>
+        </div>
         <h1 class="text-center font-bold text-2xl">Trading212</h1>
         <p>
             1. Make an API Key. <br>
@@ -85,6 +91,12 @@
 <input type="checkbox" id="kraken_exchange_modal" class="modal-toggle">
 <div class="modal" role="dialog">
     <div class="modal-box bg-base-200">
+        <div role="alert" class="alert alert-warning">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>Your API Key is invalid or does not have the required permissions.</span>
+        </div>
         <h1 class="text-center font-bold text-2xl">Kraken Crypto Exchange</h1>
         <p>
             1. Make an API Key. <br>
