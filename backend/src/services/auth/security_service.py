b1a86d995fa36b1c6e9d8332fc91afa934b472c1
Dominic <DominicArda@icloud.com>
2025-06-25 13:22:54 +0100

    #27 Create method for generating encryption key from password

--- backend/src/services/auth/security_service.py
+++ backend/src/services/auth/security_service.py
@@ -1,7 +1,32 @@
+import base64
 from bcrypt import hashpw, gensalt, checkpw
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
 
 def hash_password(password : str) -> str:
     return hashpw(password.encode("utf-8"), gensalt()).decode("utf-8")
 
 def verify_password(plain_password : str, hashed_password : str) -> bool:
     return checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
+
+def generate_salt() -> bytes:
+    """
+    Create a random salt.
+
+    :return: Random salt.
+    """
+    return gensalt()
+
+def generate_key_from_password(password : str, salt : bytes) -> bytes:
+    """
+    Derive an encryption key from a password.
+    """
+    key = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000
+    )
+    return base64.urlsafe_b64encode(key.derive(password.encode("utf-8")))
