7ef1129f004d38b4045b5653a8830166955cf99a
Dominic <DominicArda@icloud.com>
2025-06-25 21:38:44 +0100

    #27 Create test case for encrypting and decrypting with symmetric encryption, 100% line coverage

--- backend/test/services/auth/test_security_service.py
+++ backend/test/services/auth/test_security_service.py
@@ -1,3 +1,6 @@
+import pytest
+from cryptography.fernet import InvalidToken
+
 from backend.src.services.auth.security_service import *
 
 
@@ -22,3 +25,18 @@ def test_generate_key_from_password():
 
     assert key1 != key2
     assert isinstance(key1, bytes)
+
+def test_encrypt_and_decrypt():
+    salt = generate_salt()
+
+    password = "unittesting"
+
+    plaintext = "Inconceivable!"
+
+    ciphertext = encrypt_data(plaintext, password, salt)
+
+    assert plaintext != ciphertext
+    assert plaintext == decrypt_data(ciphertext, password, salt)
+
+    with pytest.raises(InvalidToken):
+        decrypt_data(ciphertext, "WrongPassword?", salt)
