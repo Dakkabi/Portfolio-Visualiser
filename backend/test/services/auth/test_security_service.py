435b336a8ffe41073f895c247fae2cd9da02aa14
Dominic <DominicArda@icloud.com>
2025-06-25 13:23:07 +0100

    #27 Create test cases for generating key from password

--- backend/test/services/auth/test_security_service.py
+++ backend/test/services/auth/test_security_service.py
@@ -1,4 +1,5 @@
-from backend.src.services.auth.security_service import hash_password, verify_password
+from backend.src.services.auth.security_service import *
+
 
 def test_hash_and_verify():
     password1 = "unittesting"
@@ -6,3 +7,18 @@ def test_hash_and_verify():
 
     assert verify_password(password1, hash_password(password1))
     assert not verify_password(password1, hash_password(password2))
+
+def test_generate_key_from_password():
+    password1 = "unittesting"
+    password2 = "iscool"
+
+    salt = generate_salt()
+    new_salt = generate_salt()
+
+    assert new_salt != salt
+
+    key1 = generate_key_from_password(password1, salt)
+    key2 = generate_key_from_password(password2, salt)
+
+    assert key1 != key2
+    assert isinstance(key1, bytes)
