9c1bbf7079b3159ca2923900cad88cad77671c12
Dominic <DominicArda@icloud.com>
2025-06-29 20:56:11 +0100

    #29 Create get broker by name operation for broker table

--- backend/src/database/crud/broker_model.py
+++ backend/src/database/crud/broker_model.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import Session
 from backend.src.database.models.broker_model import Broker
 from backend.src.schemas.model.broker_schema import BrokerCreate
 
+def get_broker(db: Session, broker_name: str):
+    return db.query(Broker).filter(Broker.name == broker_name).first()
+
 def add_broker(db : Session, broker: BrokerCreate):
     db_broker = Broker(
         name=broker.name
