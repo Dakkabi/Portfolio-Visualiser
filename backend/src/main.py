4f7293e3369a3651fcfe86b1a48d626559f1b130
Dominic <DominicArda@icloud.com>
2025-06-29 21:14:59 +0100

    #29 Add brokers from file on FastAPI startup

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
+
+from backend.src.database.crud.broker_model import add_broker_from_file
+from backend.src.database.session import get_db
 from backend.src.routes.auth.auth_router import auth_router
 from backend.src.routes.models.user_router import user_router
 
@@ -9,4 +12,6 @@ app.include_router(auth_router, prefix="/api")
 app.include_router(user_router, prefix="/api")
 
 if __name__ == "__main__":
+    add_broker_from_file(next(get_db()))
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
