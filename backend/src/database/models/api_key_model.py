eedeb706d139303c7dbf13fe349614b33a3aa69d
Dominic <DominicArda@icloud.com>
2025-06-29 21:15:43 +0100

    #29 Create columns for user.id and brokers.name and set as foreign keys

--- backend/src/database/models/api_key_model.py
+++ backend/src/database/models/api_key_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.src.database.session import Base
@@ -12,4 +12,7 @@ class ApiKey(Base):
     private_key: Mapped[str] = mapped_column(String(128))
 
     user: Mapped["User"] = relationship("User", back_populates="api_keys")
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+
     broker: Mapped["Broker"] = relationship("Broker", back_populates="api_keys")
+    broker_name: Mapped[str] = mapped_column(String(128), ForeignKey("brokers.name"))
