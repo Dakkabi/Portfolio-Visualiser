4dfa6c89e6dd2b3978339ae244d1d5fdbdcbe5cb
Dominic <DominicArda@icloud.com>
2025-06-29 20:46:05 +0100

    #29 Use primitive type 'str' rather than 'String'

--- backend/src/database/models/broker_model.py
+++ backend/src/database/models/broker_model.py
@@ -7,6 +7,6 @@ from backend.src.database.session import Base
 class Broker(Base):
     __tablename__ = 'broker'
 
-    name: Mapped[String] = MappedColumn(String(64), unique=True, primary_key=True)
+    name: Mapped[str] = MappedColumn(String(64), unique=True, primary_key=True)
 
     api_keys: Mapped[list["ApiKey"]] = relationship("ApiKey", back_populates="broker")
