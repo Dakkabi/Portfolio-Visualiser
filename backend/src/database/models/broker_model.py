192129713e2ffebd6024c5c92123d6e051ba2ced
Dominic <DominicArda@icloud.com>
2025-06-29 20:32:56 +0100

    #29 Make broker name primary key as its unique

--- backend/src/database/models/broker_model.py
+++ backend/src/database/models/broker_model.py
@@ -7,7 +7,6 @@ from backend.src.database.session import Base
 class Broker(Base):
     __tablename__ = 'broker'
 
-    id: Mapped[int] = MappedColumn(Integer, primary_key=True)
-    name: Mapped[String] = MappedColumn(String(64), unique=True)
+    name: Mapped[String] = MappedColumn(String(64), unique=True, primary_key=True)
 
     api_keys: Mapped[list["ApiKey"]] = relationship("ApiKey", back_populates="broker")
