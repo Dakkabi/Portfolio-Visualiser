8f398e951fcaeb0fcdc4dc064c28bf8f157de053
Dominic <DominicArda@icloud.com>
2025-06-30 22:07:16 +0100

    #32 Test with 100% line coverage for Broker classes

--- backend/test/database/crud/test_broker_model.py
+++ backend/test/database/crud/test_broker_model.py
@@ -1,4 +1,10 @@
+from backend.src.database.crud.broker_model import get_brokers, add_broker_from_file
 from backend.test.database.test_session import db
 
 def test_add_broker_from_file(db):
-    pass
+    # Ensure that the db is empty before,
+    # Then add brokers from file
+    # Assert that it is not empty.
+    assert get_brokers(db) == []
+    add_broker_from_file(db)
+    assert not get_brokers(db) == []
