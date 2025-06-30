b5c17ad8fc4894a3da71c28f65ba544d2d4dee43
Dominic <DominicArda@icloud.com>
2025-06-30 20:59:07 +0100

    #30 Create 'SELECT * FROM brokers' operation

--- backend/src/database/crud/broker_model.py
+++ backend/src/database/crud/broker_model.py
@@ -5,6 +5,9 @@ from sqlalchemy.orm import Session
 from backend.src.database.models.broker_model import Broker
 from backend.src.schemas.model.broker_schema import BrokerCreate
 
+def get_brokers(db : Session):
+    return db.query(Broker).all()
+
 def get_broker(db: Session, broker_name: str):
     return db.query(Broker).filter(Broker.name == broker_name).first()
 
