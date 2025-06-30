c48f2187ec22edbc0f6aaeddc9f5f3c1d1a0e5e8
Dominic <DominicArda@icloud.com>
2025-06-30 22:00:41 +0100

    #32 Nuclear Option: Use raw SQL to drop and create the schema to avoid SQLAlchemy dependency handling

--- backend/test/database/test_session.py
+++ backend/test/database/test_session.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from backend.src.core.config import settings
 from backend.src.database.session import get_db, Base
 import pytest
@@ -10,15 +10,17 @@ def db() -> Session:
     DATABASE_TEST_URI = settings.SQLALCHEMY_TEST_DATABASE_URI
     engine = create_engine(str(DATABASE_TEST_URI))
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    with engine.begin() as conn:
+        conn.execute(text("DROP SCHEMA public CASCADE"))
+        conn.execute(text("CREATE SCHEMA public"))
+        Base.metadata.create_all(bind=conn)
+
     db_session = SessionLocal()
-
-    base = Base()
-    base.metadata.drop_all(bind=engine)
-    base.metadata.create_all(bind=engine)
-
-    yield db_session
-
-    db_session.close()
+    try:
+        yield db_session
+    finally:
+        db_session.close()
 
 def test_db(prod_db = next(get_db())):
     assert prod_db is not None
