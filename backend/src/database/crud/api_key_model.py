d71048110b5621477b2538309439ed2ddefa556f
Dominic <DominicArda@icloud.com>
2025-07-01 10:07:54 +0100

    #33 Create api key operation

--- backend/src/database/crud/api_key_model.py
+++ backend/src/database/crud/api_key_model.py
@@ -1,7 +1,17 @@
 from sqlalchemy.orm import Session
 
 from backend.src.schemas.model.api_key_schema import ApiKeyCreate
+from backend.src.services.auth.security_service import encrypt_data
 
 
-def create_api_key(db : Session, new_api_key : ApiKeyCreate, secret_key : bytes):
-    pass
+def create_api_key(db: Session, new_api_key: ApiKeyCreate, secret_key: str):
+    db_api_key = ApiKeyCreate(
+        api_key=encrypt_data(new_api_key.api_key, secret_key),
+        private_key=encrypt_data(new_api_key.private_key, secret_key),
+        user_id=new_api_key.user_id,
+        broker_name=new_api_key.broker_name
+    )
+    db.add(db_api_key)
+    db.commit()
+    db.refresh(db_api_key)
+    return db_api_key
