4f898f30d0e1a4fbeb062150173817c0f54c3b5a
Dominic <DominicArda@icloud.com>
2025-07-01 23:16:19 +0100

    #36 follow keep it simple, stupid regarding passwords, send password as string.

--- backend/src/routes/auth/security_router.py
+++ backend/src/routes/auth/security_router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 
 from backend.src.database.models.user_model import User
-from backend.src.schemas.auth.security_schema import ResponseSchema, DeriveKeyRequest
 from backend.src.services.auth.auth_service import get_current_user
 from backend.src.services.auth.security_service import generate_key_from_password
 
@@ -10,17 +9,6 @@ security_router = APIRouter(
     tags=["Encryption"]
 )
 
-@security_router.post("/derive-key", response_model=ResponseSchema)
-def derive_key(request : DeriveKeyRequest, user : User = Depends(get_current_user)):
-    """
-    Derive an encryption key from a password.
-
-    This endpoint does not mutate server-side resources, however to avoid logging the password in the url,
-    we are using a query instead.
-
-    :param request: A query schema for the password.
-    :param user: The authenticated user to derive an encryption key for.
-
-    :return: An encryption key to be stored.
-    """
-    return generate_key_from_password(user.id, request.password)
+@security_router.get("/derive-key")
+def derive_key(password : str, user : User = Depends(get_current_user)):
+    return generate_key_from_password(user.id, password)
