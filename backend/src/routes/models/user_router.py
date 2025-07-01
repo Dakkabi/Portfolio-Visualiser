fed1fe12fe8513d91b4f6506d37e20c399a0dd86
Dominic <DominicArda@icloud.com>
2025-07-01 22:35:00 +0100

    #36 Organise endpoints in ascending order of GET then POST.

--- backend/src/routes/models/user_router.py
+++ backend/src/routes/models/user_router.py
@@ -12,13 +12,13 @@ user_router = APIRouter(
     tags=["users"]
 )
 
-@user_router.get("/me", response_model=UserSchema)
-def me(current_user: User = Depends(get_current_active_user)):
-    return current_user
-
 @user_router.get("/", response_model=List[UserSchema])
 def get_all_users(db: Session = Depends(get_db)):
     return get_users(db)
+
+@user_router.get("/me", response_model=UserSchema)
+def me(current_user: User = Depends(get_current_active_user)):
+    return current_user
 
 @user_router.get("/{user_id}", response_model=UserSchema)
 def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
