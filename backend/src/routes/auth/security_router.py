2cdbd7efa89fc03f8ef81b5305bb398706a36a53
Dominic <DominicArda@icloud.com>
2025-07-02 22:22:55 +0100

    #36 Use correct get_current_active_user dependency

--- backend/src/routes/auth/security_router.py
+++ backend/src/routes/auth/security_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Security
 
 from backend.src.database.models.user_model import User
 from backend.src.schemas.auth.security_schema import SecurityUserPassword
-from backend.src.services.auth.auth_service import get_current_user
+from backend.src.services.auth.auth_service import get_current_active_user
 from backend.src.services.auth.security_service import generate_key_from_password
 
 security_router = APIRouter(
@@ -11,5 +11,5 @@ security_router = APIRouter(
 )
 
 @security_router.post("/derive-key")
-def derive_key(password: SecurityUserPassword, user: User = Depends(get_current_user)):
+def derive_key(password: SecurityUserPassword, user: User = Depends(get_current_active_user)):
     return generate_key_from_password(user.id, password.password)
