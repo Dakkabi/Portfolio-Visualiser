4847a8952d5deeaff92186a0b0f7a2567e0de982
Dominic <DominicArda@icloud.com>
2025-07-02 17:38:52 +0100

    #36 Refactor broker model method names to standard '{method}_db_broker'

--- backend/src/database/crud/broker_model.py
+++ backend/src/database/crud/broker_model.py
@@ -6,13 +6,13 @@ from backend.src.database.models.broker_model import Broker
 from backend.src.schemas.model.broker_schema import BrokerCreate
 
 
-def get_brokers(db : Session):
+def get_db_brokers(db : Session):
     return db.query(Broker).all()
 
-def get_broker(db: Session, broker_name: str):
+def get_db_broker(db: Session, broker_name: str):
     return db.query(Broker).filter(Broker.name == broker_name).first()
 
-def add_broker(db : Session, broker: BrokerCreate):
+def add_db_broker(db : Session, broker: BrokerCreate):
     db_broker = Broker(
         name=broker.name
     )
@@ -33,5 +33,5 @@ def add_broker_from_file(db : Session, file_path : str = None):
     broker_names = [name.strip() for name in broker_names]
 
     for name in broker_names:
-        if get_broker(db, name) is None:
-            db_broker = add_broker(db, BrokerCreate(name=name))
+        if get_db_broker(db, name) is None:
+            add_db_broker(db, BrokerCreate(name=name))
--- backend/src/routes/models/broker_router.py
+++ backend/src/routes/models/broker_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from backend.src.database.crud.broker_model import get_brokers
+from backend.src.database.crud.broker_model import get_db_brokers
 from backend.src.database.session import get_db
 
 broker_router = APIRouter(
@@ -11,4 +11,4 @@ broker_router = APIRouter(
 
 @broker_router.get("/")
 def get_all_brokers(db : Session = Depends(get_db)):
-    return get_brokers(db)
+    return get_db_brokers(db)
