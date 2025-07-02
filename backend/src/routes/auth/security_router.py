75fe71ee207b8df1473e09bf858c8bde2d5936aa
Dominic <DominicArda@icloud.com>
2025-07-02 18:27:25 +0100

    #36 Use schema for password rather than query

--- backend/src/routes/auth/security_router.py
+++ backend/src/routes/auth/security_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from backend.src.database.models.user_model import User
+from backend.src.schemas.auth.security_schema import SecurityUserPassword
 from backend.src.services.auth.auth_service import get_current_user
 from backend.src.services.auth.security_service import generate_key_from_password
 
@@ -10,5 +11,5 @@ security_router = APIRouter(
 )
 
 @security_router.get("/derive-key")
-def derive_key(password : str, user : User = Depends(get_current_user)):
-    return generate_key_from_password(user.id, password)
+def derive_key(credentials: SecurityUserPassword, user: User = Depends(get_current_user)):
+    return generate_key_from_password(user.id, credentials.password)
