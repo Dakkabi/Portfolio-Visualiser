c786b4d4abe7c8e1946f57800d20ba2f4a08d146
Dominic <DominicArda@icloud.com>
2025-07-02 21:57:23 +0100

    #36 Prepare test file for api_key_router

---
+++ backend/test/routes/models/test_api_key_router.py
@@ -0,0 +1,17 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from backend.src.database.session import get_db
+from backend.src.main import app
+
+client = TestClient()
+url_prefix = "/api/api-keys"
+
+@pytest.fixture(autouse=True)
+def override_dependency_db(db):
+    def _get_db_override():
+        yield db
+    app.dependency_overrides[get_db] = _get_db_override
+
+def create_and_delete_api_keys():
+    pass
