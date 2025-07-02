8be301fdeb3ca438962672efeb3ec759ebf049f0
Dominic <DominicArda@icloud.com>
2025-07-02 22:29:26 +0100

    #36 Remove unneeded fixture and imports

--- backend/test/routes/models/test_api_key_router.py
+++ backend/test/routes/models/test_api_key_router.py
@@ -1,17 +1,9 @@
-import pytest
 from fastapi.testclient import TestClient
 
-from backend.src.database.session import get_db
-from backend.src.main import app
+from backend.test.conftest import *
 
 client = TestClient(app)
 url_prefix = "/api/api-keys"
-
-@pytest.fixture(autouse=True)
-def override_dependency_db(db):
-    def _get_db_override():
-        yield db
-    app.dependency_overrides[get_db] = _get_db_override
 
 def create_and_delete_api_keys():
     pass
