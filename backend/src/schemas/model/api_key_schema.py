8b43f89e5d0712651e6bd2ec390c014192d896e9
Dominic <DominicArda@icloud.com>
2025-07-02 13:44:52 +0100

    #36 Add schemas for Update and Delete operations, add Response model schema displaying encrypted keys

--- backend/src/schemas/model/api_key_schema.py
+++ backend/src/schemas/model/api_key_schema.py
@@ -7,7 +7,17 @@ class ApiKeyBase(BaseModel):
 class ApiKeySchema(ApiKeyBase):
     user_id : int
 
+class ApiKeyAuthSchema(ApiKeySchema):
+    encrypted_api_key: str
+    encrypted_private_key: str
+
 class ApiKeyCreate(ApiKeyBase):
     api_key: str
     private_key: str
     secret_key: str
+
+class ApiKeyUpdate(ApiKeyCreate):
+    pass
+
+class ApiKeyDelete(ApiKeyCreate):
+    pass
