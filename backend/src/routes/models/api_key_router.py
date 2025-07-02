62de39caa57f37e85bb06a45d0e9a14043ebdcb7
Dominic <DominicArda@icloud.com>
2025-07-02 16:26:16 +0100

    #36 Adhere to separation of concerns, raise an exception if attempting to create a key that already exists or change a non-existent key

--- backend/src/routes/models/api_key_router.py
+++ backend/src/routes/models/api_key_router.py
@@ -1,11 +1,12 @@
-from typing import List
+from typing import List, Any
 
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 
 from backend.src.database.crud.api_key_model import *
 from backend.src.database.models.user_model import User
 from backend.src.database.session import get_db
-from backend.src.schemas.model.api_key_schema import ApiKeySchema, ApiKeyCreate, ApiKeyAuthSchema, ApiKeyUpdate
+from backend.src.schemas.model.api_key_schema import *
 from backend.src.services.auth.auth_service import get_current_active_user
 
 api_key_router = APIRouter(
@@ -13,43 +14,28 @@ api_key_router = APIRouter(
     tags=["API Keys"]
 )
 
-@api_key_router.get("/", response_model=List[ApiKeyAuthSchema])
+@api_key_router.get("/", response_model=List[ApiKeySensitiveSchema])
 def get_my_api_keys(
         db: Session = Depends(get_db),
         current_user: User = Depends(get_current_active_user)
 ):
     response = get_db_api_keys_by_user_id(db, current_user.id)
 
-    for schema in response:
-        # It is an ApiKey schema, not a dict
-        schema.encrypted_api_key = schema.api_key
-        schema.encrypted_private_key = schema.private_key
-
-        del schema.api_key
-        del schema.private_key
-
     return response
 
-@api_key_router.post("/", response_model=ApiKeySchema)
+@api_key_router.post("/", response_model=ApiKeySensitiveSchema)
 def add_api_key(
         api_key: ApiKeyCreate,
         db : Session = Depends(get_db),
         current_user: User = Depends(get_current_active_user)
 ):
-    db_api_key = get_db_api_key(db, api_key.user_id, api_key.broker_name)
+    db_api_key = get_db_api_key(db, current_user.id, api_key.broker_name)
     if db_api_key:
-        # Masking is required as the IDE will complain otherwise
-        new_api_key = ApiKeyUpdate(
-            api_key=api_key.api_key,
-            private_key=api_key.private_key,
-            broker_name=api_key.broker_name,
-            secret_key=api_key.secret_key
-        )
-        return update_db_api_key(db, new_api_key, current_user)
+        raise HTTPException(status_code=409, detail="Key already exists")
 
     return create_db_api_key(db, api_key, current_user)
 
-@api_key_router.put("/")
+@api_key_router.put("/", response_model=ApiKeySensitiveSchema)
 def update_api_key(
         new_api_key: ApiKeyUpdate,
         db: Session = Depends(get_db),
@@ -57,7 +43,7 @@ def update_api_key(
 ):
     db_api_key = get_db_api_key(db, current_user.id, new_api_key.broker_name)
     if not db_api_key:
-        return create_db_api_key(db, new_api_key, current_user)
+        raise HTTPException(status_code=404, detail="Key not found")
 
     return update_db_api_key(db, new_api_key, current_user)
 
