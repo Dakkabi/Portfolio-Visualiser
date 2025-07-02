32ef7d2450080f868d57fa444117fd481b962052
Dominic <DominicArda@icloud.com>
2025-07-02 21:53:39 +0100

    #36 get_brokers_from_file is called at test-db setup, therefore only need to check if table contains entries

--- backend/test/database/crud/test_broker_model.py
+++ backend/test/database/crud/test_broker_model.py
@@ -2,9 +2,6 @@ from backend.src.database.crud.broker_model import get_db_brokers, add_broker_fr
 from backend.test.database.test_session import db
 
 def test_add_broker_from_file(db):
-    # Ensure that the db is empty before,
-    # Then add brokers from file
-    # Assert that it is not empty.
-    assert get_db_brokers(db) == []
-    add_broker_from_file(db)
+    # get_brokers_from_file is called on db setup
+
     assert not get_db_brokers(db) == []
