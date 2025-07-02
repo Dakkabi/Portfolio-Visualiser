4ebb39f0382cd3a49e995c46adfccf1b5924922b
Dominic <DominicArda@icloud.com>
2025-07-02 14:20:50 +0100

    #36 Create update_api_key endpoint

--- backend/src/routes/models/api_key_router.py
+++ backend/src/routes/models/api_key_router.py
@@ -8,7 +8,7 @@ from backend.src.database.crud.api_key_model import create_api_key, get_api_keys
     delete_db_api_key
 from backend.src.database.models.user_model import User
 from backend.src.database.session import get_db
-from backend.src.schemas.model.api_key_schema import ApiKeySchema, ApiKeyCreate, ApiKeyAuthSchema
+from backend.src.schemas.model.api_key_schema import ApiKeySchema, ApiKeyCreate, ApiKeyAuthSchema, ApiKeyUpdate
 from backend.src.services.auth.auth_service import get_current_active_user
 
 api_key_router = APIRouter(
@@ -40,6 +40,18 @@ def add_api_key(
         current_user: User = Depends(get_current_active_user)
 ):
     return create_api_key(db, api_key, current_user)
+
+@api_key_router.put("/")
+def update_api_key(
+        new_api_key: ApiKeyUpdate,
+        db: Session = Depends(get_db),
+        current_user: User = Depends(get_current_active_user)
+):
+    db_api_key = get_api_key(db, new_api_key.user_id, new_api_key.broker_name)
+    if not db_api_key:
+        return create_api_key(db, new_api_key, current_user)
+
+    return update_api_key(new_api_key, db, current_user)
 
 @api_key_router.delete("/{broker_name}", response_model=ApiKeySchema)
 def delete_api_key(
