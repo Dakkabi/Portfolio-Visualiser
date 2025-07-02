1a2c8f0d2df83a4185f7ece0ed6a1a6561286989
Dominic <DominicArda@icloud.com>
2025-07-02 12:08:14 +0100

    #36 Use authenticated user's id, rather than user_id passed in request body

--- backend/src/database/crud/api_key_model.py
+++ backend/src/database/crud/api_key_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 
 from backend.src.database.models.api_key_model import ApiKey
+from backend.src.database.models.user_model import User
 from backend.src.schemas.model.api_key_schema import ApiKeyCreate
 from backend.src.services.auth.security_service import encrypt_data
 
@@ -10,11 +11,11 @@ def get_api_keys(db : Session):
 def get_api_key(db: Session, user_id: int, broker_name: str):
     return db.query(ApiKey).filter_by(user_id=user_id, broker_name=broker_name).first()
 
-def create_api_key(db: Session, new_api_key: ApiKeyCreate):
+def create_api_key(db: Session, new_api_key: ApiKeyCreate, current_user: User):
     db_api_key = ApiKey(
         api_key=encrypt_data(new_api_key.api_key, new_api_key.secret_key),
         private_key=encrypt_data(new_api_key.private_key, new_api_key.secret_key),
-        user_id=new_api_key.user_id,
+        user_id=current_user.id,
         broker_name=new_api_key.broker_name
     )
     db.add(db_api_key)
