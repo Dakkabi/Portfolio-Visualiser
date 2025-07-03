e330dcfb55f7c59b3be5b08c72ac29f02b556b52
Dominic <DominicArda@icloud.com>
2025-07-03 21:59:16 +0100

    #23 Add methods for personal portfolio section

--- backend/src/services/brokers/trading212_api.py
+++ backend/src/services/brokers/trading212_api.py
@@ -5,7 +5,7 @@ class Trading212(AbstractBrokerAPI):
 
     @staticmethod
     def verify_api_key(api_key: str):
-        # fetch_account_data has the lowest rate limit at 1/2
+        # fetch_account_data has the lowest rate limit at 1/2s
         try:
             Trading212(api_key=api_key).fetch_account_data()
         except Exception as e:
@@ -18,3 +18,16 @@ class Trading212(AbstractBrokerAPI):
 
     def fetch_account_metadata(self):
         return super().requests(method="GET", path="/api/v0/equity/account/info")
+
+    # Personal Portfolio
+    def fetch_all_open_positions(self):
+        return super().requests(method="GET", path="/api/v0/equity/portfolio")
+
+    def search_for_a_specific_position_by_ticker(self, ticker: str):
+        payload = {
+            "ticker": ticker
+        }
+        return super().requests(method="POST", path="/api/v0/equity/portfolio/ticker", params=payload)
+
+    def fetch_a_specific_position(self, ticker: str):
+        return super().requests(method="GET", path=f"/api/v0/equity/portfolio/{ticker}")
