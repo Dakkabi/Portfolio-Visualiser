628d3245050c310f917eb5d76c40fd8cd18c9c4d
Dominic <DominicArda@icloud.com>
2025-07-03 20:57:12 +0100

    #23 Add requests method implementation and add attribute for base domain

--- backend/src/services/brokers/broker_api.py
+++ backend/src/services/brokers/broker_api.py
@@ -3,6 +3,8 @@ from typing import Optional
 import requests
 
 class AbstractBroker:
+    base_domain = ""
+
     def __init__(self, api_key: str, private_key: Optional[str]):
         self.api_key = api_key
         self.private_key = private_key
@@ -18,5 +20,34 @@ class AbstractBroker:
         """
         raise NotImplementedError()
 
-    def requests(self):
-        pass
+    def requests(self, method: str, path: str, params: Optional[dict] = None) -> dict:
+        """
+        Send a request to the API endpoint.
+
+        :param method: The HTTP method to use.
+        :param path: The API endpoint path.
+        :param params: Additional query/body parameters.
+        :return: Response from the API endpoint.
+
+        :raises ValueError: Unknown method name.
+        :raises HTTPError: An HTTP error occurred.
+        """
+
+        url = self.base_domain + path
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": {self.api_key}
+        }
+
+        if method == "GET":
+            response = requests.get(url, headers=headers, params=params)
+
+        elif method == "POST":
+            response = requests.post(url, headers=headers, json=params)
+
+        else:
+            raise ValueError(f"Unknown method name: {method}")
+
+        response.raise_for_status()
+        return response.json()
