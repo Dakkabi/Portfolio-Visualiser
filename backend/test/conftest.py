9ce5d27b828c986e4b9287f5aa2403ba7fca2cfd
Dominic <DominicArda@icloud.com>
2025-07-04 21:11:51 +0100

    #39 Make dummy_user return an actual user usable for testing

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 
+from backend.src.database.crud.user_crud import create_db_user
 from backend.src.database.models.user_model import User
 from backend.src.database.session import get_db
 from backend.src.main import app
+from backend.src.schemas.model.user_schema import UserCreate
 from backend.src.services.auth.auth_service import get_current_active_user
 from backend.test.database.test_session import db
 
@@ -11,9 +13,15 @@ from backend.test.database.test_session import db
 def global_secret_key():
     return "96U4uCt0mB4X2gRFvdAUNVI0S8OsLVnzjxuy6tFaWv0="
 
-@pytest.fixture(scope="session")
-def dummy_active_user():
-    return User(id=3, email="dummy@user.com", password="not_real")
+@pytest.fixture
+def dummy_active_user(db):
+    return create_db_user(
+        db,
+        UserCreate(
+            email="dummy@user.com",
+            password="password"
+        )
+    )
 
 @pytest.fixture(scope="function", autouse=True)
 def override_dependencies(dummy_active_user, db):
