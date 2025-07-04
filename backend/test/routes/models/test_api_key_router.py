24368d3d1885e30beaba891c215195944ad4dfc7
Dominic <DominicArda@icloud.com>
2025-07-04 22:37:48 +0100

    #39 Create test cases for all raised exceptions apart from testing for key conflict. Use dummy_active_user rather than creating new user fixture.

--- backend/test/routes/models/test_api_key_router.py
+++ backend/test/routes/models/test_api_key_router.py
@@ -1,32 +1,82 @@
 import requests.exceptions
-from fastapi import HTTPException
 from fastapi.testclient import TestClient
 
-from backend.src.database.crud.user_crud import create_db_user
 from backend.src.routes.models.api_key_router import *
-from backend.src.schemas.model.user_schema import UserCreate
 from backend.test.conftest import *
 
 client = TestClient(app)
 url_prefix = "/api/api-keys"
 
-@pytest.fixture
-def setup_users(db : Session):
-    db_user = create_db_user(
-        db,
-        UserCreate(
-        email="api@key.com",
-        password="router"
-        )
-    )
-    return db_user
-
-# Must come before api_key_failure
-def test_add_api_key_success(monkeypatch, db, setup_users):
+def test_add_api_key_success(monkeypatch, db, dummy_active_user, global_secret_key):
+    """Must come before test_add_api_key_failure"""
     broker_name = "Trading212"
     monkeypatch.setitem(registry, broker_name, lambda key: None)
 
-def test_add_api_key_failure(monkeypatch, db, setup_users):
+    client.post(
+        f"{url_prefix}/",
+        json={
+            "api_key": {
+                "broker_name": broker_name,
+                "api_key": "valid",
+                "private_key": "valid"
+            },
+            "secret_key": {
+                "secret_key": global_secret_key
+            }
+        }
+    )
+    assert get_my_api_keys(db, dummy_active_user) is not None
+
+
+def test_add_api_key_failure(monkeypatch, db, global_secret_key):
+    """Must come after test_add_api_key_success"""
     broker_name = "Trading212"
+    broker_name2 = "Kraken"
     monkeypatch.setitem(registry, broker_name, lambda key: requests.exceptions.HTTPError)
+    monkeypatch.setitem(registry, broker_name2, lambda key: requests.exceptions.HTTPError)
+
+    json = {
+            "api_key": {
+                "broker_name": broker_name,
+                "api_key": "valid",
+                "private_key": "valid"
+            },
+            "secret_key": {
+                "secret_key": global_secret_key
+            }
+        }
+
+    json_with_no_broker_name = {
+            "api_key": {
+                "broker_name": "unknown broker",
+                "api_key": "valid",
+                "private_key": "valid"
+            },
+            "secret_key": {
+                "secret_key": global_secret_key
+            }
+        }
+    response = client.post(
+        f"{url_prefix}/",
+        json=json_with_no_broker_name
+    )
+    assert response.status_code == 404
+
+
+
+    json_with_another_exchange = {
+            "api_key": {
+                "broker_name": broker_name2,
+                "api_key": "valid",
+                "private_key": "valid"
+            },
+            "secret_key": {
+                "secret_key": global_secret_key
+            }
+        }
+    response = client.post(
+        f"{url_prefix}/",
+        json=json_with_another_exchange
+    )
+    assert response.status_code == 400
 
