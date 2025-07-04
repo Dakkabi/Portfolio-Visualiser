d617e24636063265fb055c00a715843f130532a9
Dominic <DominicArda@icloud.com>
2025-07-04 13:05:38 +0100

    #39 Remove reference to brokerAPI router

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,7 +6,6 @@ from backend.src.database.session import get_db
 from backend.src.routes.auth.auth_router import auth_router
 from backend.src.routes.auth.security_router import security_router
 from backend.src.routes.models.api_key_router import api_key_router
-from backend.src.routes.models.broker_router import broker_router
 from backend.src.routes.models.user_router import user_router
 
 app = FastAPI()
@@ -15,7 +14,6 @@ app.include_router(auth_router, prefix="/api")
 app.include_router(user_router, prefix="/api")
 app.include_router(api_key_router, prefix="/api")
 app.include_router(security_router, prefix="/api")
-app.include_router(broker_router, prefix="/api")
 
 if __name__ == "__main__":
     add_broker_from_file(next(get_db()))
