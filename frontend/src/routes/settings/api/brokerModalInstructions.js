d7db881a19b817117a5f2421aa816540ac612797
Dominic <DominicArda@icloud.com>
2025-07-06 21:52:25 +0100

    #41 Add instructions for Kraken API Key generation including images

--- frontend/src/routes/settings/api/brokerModalInstructions.js
+++ frontend/src/routes/settings/api/brokerModalInstructions.js
@@ -12,5 +12,17 @@ export let brokersInfo = [
             `4. Upon creation, copy it using the Copy icon, and input it into the box below.`,
             `<img src="/brokers/trading212/trading212_api_key_save.png" alt="">`
         ]
+    },
+    {
+        id: 'kraken_exchange_modal',
+        title: "Kraken Exchange",
+        instructions: [
+            `1. On the Kraken main page, click your User icon, go to Settings. Choose <b>Connections & API</b> tab, and click Create API Key for Spot Trading API.`,
+            `<img src="/brokers/kraken/kraken_create_api_key_instruction.png" alt="" >`,
+            `2. For permissions, all would be best in terms of least issues, however the image below offers the bare minimum requirement.`,
+            `<img src="/brokers/kraken/kraken_permissions_instruction.png" alt="" >`,
+            `3. Once created, copy the API key and Private key into their respective boxes below, they are not interchangeable, and cannot be viewed once closed!`,
+            `<img src="/brokers/kraken/kraken_save_api_keys.png" alt="" >`
+        ]
     }
 ]
