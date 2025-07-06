1e303624053be47836f9bd587eccaeba89501d4e
Dominic <DominicArda@icloud.com>
2025-07-06 20:53:45 +0100

    #41 Create a separate js file for brokers instructions information, separation of concerns

---
+++ frontend/src/routes/settings/api/brokerModalInstructions.js
@@ -0,0 +1,10 @@
+export let brokersInfo = [
+    {
+        id: 'trading212_modal',
+        title: 'Trading212',
+        instructions: [
+            'Make an API Key.',
+            'Put it here.'
+        ]
+    }
+]
