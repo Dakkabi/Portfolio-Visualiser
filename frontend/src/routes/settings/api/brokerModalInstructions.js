5a5fa32d7c3e2b8c552333348a53069e839ac0f0
Dominic <DominicArda@icloud.com>
2025-07-06 21:37:27 +0100

    #41 Add instruction for Trading212 including images

--- frontend/src/routes/settings/api/brokerModalInstructions.js
+++ frontend/src/routes/settings/api/brokerModalInstructions.js
@@ -3,8 +3,14 @@ export let brokersInfo = [
         id: 'trading212_modal',
         title: 'Trading212',
         instructions: [
-            'Make an API Key.',
-            'Put it here.'
+            `1. On the Trading212 main page, click the Hamburger icon to bring up an Information page.`,
+            `<img src="/brokers/trading212/trading212_settings_instruction.png" alt="">`,
+            `2. Click the Settings row, and then the API row.`,
+            `<img src="/brokers/trading212/trading212_api_creation_instruction.png" alt="">`,
+            `3. Give it a meaningful name, and please give all permissions for the least problems.`,
+            `<img src="/brokers/trading212/trading212_api_permission_instruction.png" alt="">`,
+            `4. Upon creation, copy it using the Copy icon, and input it into the box below.`,
+            `<img src="/brokers/trading212/trading212_api_key_save.png" alt="">`
         ]
     }
 ]
