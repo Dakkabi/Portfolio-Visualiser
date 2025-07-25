c055ed6231b22d679fb94050c3812948ac5cbb06
Dominic <DominicArda@icloud.com>
2025-07-25 22:19:19 +0100

    #50 Add importable path alias to axios instance "api"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [tailwindcss(), sveltekit()],
+
+    resolve: {
+        alias: {
+            '@api': path.resolve(__dirname, 'src/config/axios.config.js')
+        }
+    },
 
     server: {
         proxy: {
