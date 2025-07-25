ea0791e6ef080145435e9d2b9b7609928de200c8
Dominic <DominicArda@icloud.com>
2025-07-25 22:25:05 +0100

    #50 Create shell api class for Kraken with verify_api_key_response always returning True

---
+++ backend/src/services/brokers/kraken_api.py
@@ -0,0 +1,9 @@
+from backend.src.services.brokers.broker_api import AbstractBrokerAPI
+
+
+class Kraken(AbstractBrokerAPI):
+    base_domain = ""
+
+    @staticmethod
+    def verify_api_key_response():
+        return True
