f32709c1f4af624e401983f7e2059947fc8e43cb
Dominic <DominicArda@icloud.com>
2025-08-01 12:12:15 +0100

    #3 Add POST "/" endpoint to create user

--- backend/api/models/user_router.py
+++ backend/api/models/user_router.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
+from backend.database.crud.user_crud import create_db_user
 from backend.database.models.user_model import User
-from backend.schemas.models.user_schema import UserSchema
+from backend.database.session import get_db
+from backend.schemas.models.user_schema import UserSchema, UserCreate
 from backend.services.security.auth_service import get_current_active_user
 
 user_router = APIRouter(
@@ -12,3 +15,7 @@ user_router = APIRouter(
 @user_router.get("/me", response_model=UserSchema)
 def get_me(current_user: User = Depends(get_current_active_user)):
     return current_user
+
+@user_router.post("/", response_model=UserSchema)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    return create_db_user(db, user)
