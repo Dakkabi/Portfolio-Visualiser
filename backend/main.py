09dc034fb1e82338336caa9471a60be6bca3ea5e
Dominic <DominicArda@icloud.com>
2025-08-01 12:09:26 +0100

    #3 Attach auth_router and user_router to main FastAPI app, allow entrypoint to be invoked by running the file using uvicorn

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
 
+from backend.api.models.user_router import user_router
+from backend.api.security.auth_router import auth_router
+
 app = FastAPI()
+
+app.include_router(auth_router, prefix="/api")
+app.include_router(user_router, prefix="/api")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
