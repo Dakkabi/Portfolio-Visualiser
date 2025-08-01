5c74b9c6a31ee03112e59c545886526d0b7b1323
Dominic <DominicArda@icloud.com>
2025-08-01 13:00:57 +0100

    #8 Create signup page form

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content bg-base-100 flex-col rounded-lg">
+        <div class="text-center">
+            <h1 class="text-3xl font-bold">Sign up for Portfolio Analysis</h1>
+        </div>
+        <div class="card w-full max-w-sm shrink-0">
+            <div class="card-body">
+                <fieldset class="fieldset">
+                    <label class="label">Email</label>
+                    <input type="email" class="input" placeholder="name@domain.com" />
+                    <label class="label">Password</label>
+                    <input type="password" class="input" placeholder="Password"/>
+
+                    <button class="btn btn-neutral mt-4">Sign Up</button>
+                </fieldset>
+            </div>
+        </div>
+    </div>
+</div>
