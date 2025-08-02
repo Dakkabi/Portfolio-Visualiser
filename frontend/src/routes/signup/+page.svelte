060ef242180985b01d1de0985f646d4c70a9f8b1
Dominic <DominicArda@icloud.com>
2025-08-02 15:05:53 +0100

    #8 Create function for invoking POST "/" to create user

--- frontend/src/routes/signup/+page.svelte
+++ frontend/src/routes/signup/+page.svelte
@@ -1,3 +1,26 @@
+<script lang="ts">
+    import {api} from "../../config/axios.config.js";
+    
+    let email = '';
+    let password = '';
+
+    /**
+     * Create a user in the database.
+     *
+     * @param email The user's email.
+     * @param password The user's password.
+     */
+    function createUser(email: string, password: string) {
+        let response = api.post(
+            "/user/",
+            {
+                "email": email,
+                "password": password
+            }
+        )
+    }
+</script>
+
 <div class="hero bg-base-200 min-h-screen">
     <div class="hero-content bg-base-100 flex-col rounded-lg">
         <div class="text-center">
@@ -7,11 +30,11 @@
             <div class="card-body">
                 <fieldset class="fieldset">
                     <label class="label">Email</label>
-                    <input type="email" class="input" placeholder="name@domain.com" />
+                    <input bind:value={email} type="email" class="input" placeholder="name@domain.com" />
                     <label class="label">Password</label>
-                    <input type="password" class="input" placeholder="Password"/>
+                    <input bind:value={password} type="password" class="input" placeholder="Password"/>
 
-                    <button class="btn btn-neutral mt-4">Sign Up</button>
+                    <button on:click={() => createUser(email, password)} class="btn btn-neutral mt-4">Sign Up</button>
                 </fieldset>
             </div>
         </div>
