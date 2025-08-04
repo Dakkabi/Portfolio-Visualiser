6a56cd2ad4904f72a9d12ab052592b7b7f79fd86
Dominic <DominicArda@icloud.com>
2025-08-04 16:33:19 +0100

    #3 Create users model table

---
+++ backend/src/database/models/user_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column, Mapped
+
+from backend.src.database.session import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
