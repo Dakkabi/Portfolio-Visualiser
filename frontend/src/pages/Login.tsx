9231ad8b4617edf7b937c9c714a04bde7bcd009a
Dominic <DominicArda@icloud.com>
2025-08-06 14:57:35 +0100

    #11 Add AuthForm components with props to Login and SignUp pages

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import AuthForm from "../components/auth/AuthForm.tsx";
 
 const Login: React.FC = () => {
+    function handleLogin(email: string, password: string) {
+
+    }
+
     return (
-        <AuthForm />
+        <AuthForm
+            title="Welcome Back!"
+            buttonText="Login"
+            onButtonClick={handleLogin}
+            redirectText="Don't have an account? Click here to Sign up!"
+            redirectPath="/signup"
+        />
     );
 }
 
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import AuthForm from "../components/auth/AuthForm.tsx";
 
 const SignUp: React.FC = () => {
+    function handleSignUp(email: string, password: string) {
+
+    }
+
     return (
-        <AuthForm />
+        <AuthForm
+            title="Welcome!"
+            buttonText="Sign Up"
+            onButtonClick={handleSignUp}
+            redirectText="Already have an account with us? Click to Log back in!"
+            redirectPath="/login"
+        />
     );
 }
 
