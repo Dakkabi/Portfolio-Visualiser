c5058a5743cff61b767847d46153fbcfac1cff69
Dominic <DominicArda@icloud.com>
2025-08-09 15:56:35 +0100

    #14 Raise HTTPException in create_user if user with email already exists

--- backend/src/routes/models/user_router.py
+++ backend/src/routes/models/user_router.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from backend.src.database.crud.user_crud import create_db_user
+from backend.src.database.crud.user_crud import create_db_user, get_db_user_by_email
 from backend.src.database.models.user_model import User
 from backend.src.database.session import get_db
 from backend.src.schemas.model.user_schema import UserSchema, UserCreate
@@ -18,4 +18,6 @@ def get_user_me(current_user: User = Depends(get_current_active_user)):
 
 @user_router.post("/", response_model=UserSchema)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    if get_db_user_by_email(db, user.email):
+        raise HTTPException(status_code=400, detail="Email already registered")
     return create_db_user(db, user)
