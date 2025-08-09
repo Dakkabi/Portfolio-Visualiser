8a32d3a59f4d771909cc34a3322e2455138628d6
Dominic <DominicArda@icloud.com>
2025-08-09 15:56:13 +0100

    #14 Add CORS middleware, allow all origins, methods and headers

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from backend.src.routes.models.user_router import user_router
 from backend.src.routes.security.auth_router import auth_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 prefix = "/api"
 app.include_router(auth_router, prefix=prefix)
