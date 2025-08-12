a5c19cb8577db8ad7c2bd8af83bf2658a216ee86
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-12 19:00:13 +0100

    #3 Create dependency_override for get_current_active_user

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,10 +1,12 @@
 from typing import Generator
 
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, True_
 from sqlalchemy.orm import sessionmaker
 
 from backend.src.core.config import settings
+from backend.src.core.services.auth.auth_service import get_current_active_user
+from backend.src.database.models.user_model import User
 from backend.src.database.session import Base, get_db
 from backend.src.main import app
 
@@ -27,10 +29,18 @@ def override_get_db() -> Generator:
     finally:
         db.close()
 
+def override_get_current_active_user() -> User:
+    return User(
+        id=1,
+        email="test@test.com",
+        password="password"
+    )
+
 
 @pytest.fixture(scope="session", autouse=True)
 def override_dependency():
     app.dependency_overrides[get_db] = override_get_db
+    app.dependency_overrides[get_current_active_user] = override_get_current_active_user
 
     yield
 
