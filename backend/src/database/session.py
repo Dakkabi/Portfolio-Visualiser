8b8186884a4fba9aefcc8126b566461b5384d427
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-12 15:46:43 +0100

    #3 Add docstring to get_db

--- backend/src/database/session.py
+++ backend/src/database/session.py
@@ -1,5 +1,7 @@
+from typing import Any, Generator
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
 
 from backend.src.core.config import settings
 
@@ -9,7 +11,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 class Base(DeclarativeBase):
     pass
 
-def get_db():
+def get_db() -> Generator[Session]:
+    """Create a database session, useful for dependency injection.
+
+    :return: A bound session to the sqlalchemy engine.
+    """
     db = SessionLocal()
     try:
         yield db
