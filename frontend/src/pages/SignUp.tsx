5219763356062b88791f0deeaa5eda10c240aec7
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-13 22:03:44 +0100

    #7 Implement AuthForm component, with specified props

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,6 +1,14 @@
+import AuthForm from "../components/auth/AuthForm.tsx";
 
 function SignUp() {
-
+    return (
+        <AuthForm
+            titleText="Welcome!"
+            btnText="Sign Up"
+            redirectText="Already have an account with us? Click me!"
+            redirectPath="/login"
+        />
+    )
 }
 
 export default SignUp;
