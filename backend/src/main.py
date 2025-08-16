5d5e458d98228548b918b1852f3c39854e47ff7b
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-16 18:45:36 +0100

    #13 Create fastapi lifespan method for executing code before fastapi initialises, read file into db session

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,24 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from backend.src.database.crud.broker_crud import read_file_into_broker_table
+from backend.src.database.session import get_db
 from backend.src.routes.auth.auth_router import auth_router
 from backend.src.routes.models.user_router import user_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    db_session = next(get_db())
+
+    read_file_into_broker_table(db_session)
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
