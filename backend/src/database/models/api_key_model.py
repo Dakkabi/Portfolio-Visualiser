a04808926c380c54ab7befbdfffd986725a7785a
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-16 20:39:01 +0100

    #13 Add api_keys table with relationships with users and brokers

---
+++ backend/src/database/models/api_key_model.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from backend.src.database.models.broker_model import Broker
+from backend.src.database.models.user_model import User
+from backend.src.database.session import Base
+
+
+class ApiKey(Base):
+    __tablename__ = 'api_keys'
+
+    api_key: Mapped[str] = mapped_column(String, nullable=False)
+    private_key: Mapped[str] = mapped_column(String)
+
+    users_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    users: Mapped[User] = relationship(back_populates="api_keys")
+
+    brokers_name: Mapped[str] = mapped_column(ForeignKey("brokers.name"), primary_key=True)
+    brokers: Mapped[Broker] = relationship(back_populates="api_keys")
