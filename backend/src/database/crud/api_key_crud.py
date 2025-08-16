3cdda495e182df1cfe25c3b40e39c8839a82bdff
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-16 22:39:11 +0100

    #13 Implement get api key crud operation

--- backend/src/database/crud/api_key_crud.py
+++ backend/src/database/crud/api_key_crud.py
@@ -1,9 +1,12 @@
 from sqlalchemy.orm import Session
 
-from backend.src.core.services.auth.cryptography_service import encrypt_string
+from backend.src.core.services.auth.cryptography_service import encrypt_string, decrypt_string
 from backend.src.database.models.api_key_model import ApiKey
 from backend.src.schemas.models.api_key_schema import *
 
+def get_db_api_key(db: Session, api_key: ApiKeyRead) -> ApiKey | None:
+    """Return an api key record by broker name and user id, returns encrypted values."""
+    return db.query(ApiKey).filter(ApiKey.brokers_name == api_key.broker_name, ApiKey.users_id == api_key.user_id).first()
 
 def create_db_api_key(db: Session, api_key: ApiKeyCreate) -> ApiKey:
     """Create a new ApiKey record in the database, encrypting the values.
