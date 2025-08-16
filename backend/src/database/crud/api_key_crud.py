d168fa0dce059eefd9cf98ea07ac505ad6745cb8
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-16 22:36:47 +0100

    #14 Implement create api key record crud operation

--- backend/src/database/crud/api_key_crud.py
+++ backend/src/database/crud/api_key_crud.py
@@ -1,3 +1,24 @@
 from sqlalchemy.orm import Session
 
-from backend.src.schemas.models.api_key_schema import ApiKeyCreate
+from backend.src.core.services.auth.cryptography_service import encrypt_string
+from backend.src.database.models.api_key_model import ApiKey
+from backend.src.schemas.models.api_key_schema import *
+
+
+def create_db_api_key(db: Session, api_key: ApiKeyCreate) -> ApiKey:
+    """Create a new ApiKey record in the database, encrypting the values.
+
+    :param db: The database session.
+    :param api_key: The ApiKey record to create.
+    """
+    db_api_key = ApiKey(
+        users_id=api_key.user_id,
+        brokers_name=api_key.brokers_name,
+        api_key=encrypt_string(api_key.api_key),
+        private_key=encrypt_string(api_key.private_key),
+    )
+    db.add(db_api_key)
+    db.commit()
+    db.refresh(db_api_key)
+    return db_api_key
+
