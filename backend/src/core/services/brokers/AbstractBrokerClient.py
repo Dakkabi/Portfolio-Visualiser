d7dab96fdea2b195c9dd2443f2b9a44c3923cbc9
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-16 20:51:54 +0100

    #13 Create an abstract broker client class for use with interacting with broker's REST APIs

---
+++ backend/src/core/services/brokers/AbstractBrokerClient.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from starlette.exceptions import HTTPException
+
+
+class BrokerClient(ABC):
+
+    @abstractmethod
+    def validate_api_key(self, api_key: str, private_key: str = None) -> HTTPException | bool:
+        """Using an API endpoint, validate that the API key and optionally a Private key is valid.
+
+        :param api_key: An Api key to validate.
+        :param private_key: Optional private key to validate.
+        :return: True if the API key is valid, HTTPException otherwise.
+
+        :throws: HTTPException if the API key is invalid.
+        """
+        pass
