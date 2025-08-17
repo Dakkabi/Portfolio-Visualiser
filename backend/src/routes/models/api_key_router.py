30c248994f64e36ee4995c64bceac5fb70f1e713
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-17 13:12:15 +0100

    #13 Implement get api key record by brokers name route

--- backend/src/routes/models/api_key_router.py
+++ backend/src/routes/models/api_key_router.py
@@ -1,16 +1,24 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
 
 from backend.src.core.services.auth.auth_service import get_current_active_user
-from backend.src.database.crud.api_key_crud import create_db_api_key
+from backend.src.database.crud.api_key_crud import *
 from backend.src.database.models.user_model import User
 from backend.src.database.session import get_db
-from backend.src.schemas.models.api_key_schema import ApiKeySchema, ApiKeyRequest, ApiKeyCreate
+from backend.src.schemas.models.api_key_schema import *
 
 api_key_router = APIRouter(
     prefix="/keys",
     tags=["API Keys"]
 )
+
+@api_key_router.get("/{brokers_name}", response_model=ApiKeySchema)
+def api_key_get_by_brokers_name(
+        brokers_name: str,
+        current_user: User = Depends(get_current_active_user),
+        db: Session = Depends(get_db)
+):
+    """Fetch api key value(s) by brokers name, if exists."""
+    return get_db_api_key(db, current_user.id, brokers_name)
 
 @api_key_router.post("/", response_model=ApiKeySchema)
 def api_key_post(
@@ -18,6 +26,7 @@ def api_key_post(
         current_user: User = Depends(get_current_active_user),
         db: Session = Depends(get_db)
 ):
+    """Create a new API key record in the database."""
     api_key_create = ApiKeyCreate(
         api_key=api_key.api_key,
         private_key=api_key.private_key,
