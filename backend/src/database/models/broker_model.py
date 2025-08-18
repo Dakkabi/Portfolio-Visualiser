d895da62f9f6e3568a7852b73808e0b0ece1c726
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-18 18:39:45 +0100

    #17 Convert mapped_column row to an array of enums of type AssetType

--- backend/src/database/models/broker_model.py
+++ backend/src/database/models/broker_model.py
@@ -1,16 +1,16 @@
 from sqlalchemy import String
-from sqlalchemy.dialects.postgresql import ENUM
+from sqlalchemy.dialects.postgresql import ENUM, ARRAY
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from backend.src.database.models.api_key_model import ApiKey
 from backend.src.database.session import Base
-from backend.src.schemas.models.broker_enum import BrokerType
+from backend.src.schemas.models.broker_enum import AssetType
 
 
 class Broker(Base):
     __tablename__ = "brokers"
 
     name: Mapped[str] = mapped_column(String, primary_key=True)
-    type: Mapped[BrokerType] = mapped_column(ENUM(BrokerType, name="type_enum"), nullable=False)
+    type : Mapped[list[AssetType]] = mapped_column(ARRAY(ENUM(AssetType, native_enum=False)))
 
     api_keys: Mapped[ApiKey] = relationship(back_populates="brokers")
