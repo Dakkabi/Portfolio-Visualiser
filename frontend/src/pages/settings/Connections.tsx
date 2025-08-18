3639e3782a80aa6b305f3187d783481353f25df7
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-18 17:53:38 +0100

    #17 Create layout for Connections page

---
+++ frontend/src/pages/settings/Connections.tsx
@@ -0,0 +1,18 @@
+
+function Connections() {
+    return (
+        <div className="text-center">
+            <h1 className="text-5xl font-bold">Connections</h1>
+
+            <div className="divider"></div>
+
+            <h1 className="text-4xl font-bold">Brokers</h1>
+
+            <div className="divider"></div>
+
+            <h1 className="text-4xl font-bold">Crypto Exchanges</h1>
+        </div>
+    )
+}
+
+export default Connections;
