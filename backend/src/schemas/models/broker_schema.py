f59e4f029c382581108f762ab25a2c8bba5e4579
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-18 19:05:03 +0100

    #17 Refactor schema to include AssetType as requirement

--- backend/src/schemas/models/broker_schema.py
+++ backend/src/schemas/models/broker_schema.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
 
+from backend.src.schemas.models.broker_enum import AssetType
+
 
 class BrokerBase(BaseModel):
     name: str
+    type: list[AssetType]
+
+class BrokerCreate(BrokerBase):
+    pass
 
 class BrokerSchema(BrokerBase):
     pass
