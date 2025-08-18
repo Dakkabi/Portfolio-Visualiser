2595928a444127edfad51735aa4c644fb833463a
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-18 17:26:48 +0100

    #17 Add route to Connections setting page at "/me/connections"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import "./App.css"
 import Login from "./pages/auth/Login.tsx";
 import SignUp from "./pages/auth/SignUp.tsx";
+import Connections from "./pages/settings/Connections.tsx";
 
 function App() {
     return (
@@ -9,8 +10,12 @@ function App() {
             <Routes>
                 <Route path="/" element={<Navigate to="/login" replace />} />
 
+                {/* Auth Routes */}
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
+
+                {/* Settings Routes */}
+                <Route path="/me/connections" element={<Connections />} />
             </Routes>
         </BrowserRouter>
     )
