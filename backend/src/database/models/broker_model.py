0a3ddf393bcd5cd8bb35ef92446ba369df18e561
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-18 18:23:11 +0100

    #17 Add 'type' attribute to brokers table, enum type of BrokerType

--- backend/src/database/models/broker_model.py
+++ backend/src/database/models/broker_model.py
@@ -1,13 +1,16 @@
 from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from backend.src.database.models.api_key_model import ApiKey
 from backend.src.database.session import Base
+from backend.src.schemas.models.broker_enum import BrokerType
 
 
 class Broker(Base):
     __tablename__ = "brokers"
 
     name: Mapped[str] = mapped_column(String, primary_key=True)
+    type: Mapped[BrokerType] = mapped_column(ENUM(BrokerType, name="type_enum"), nullable=False)
 
     api_keys: Mapped[ApiKey] = relationship(back_populates="brokers")
