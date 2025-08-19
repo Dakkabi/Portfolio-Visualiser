41777e54a1796660a8b3fdf504c067945be46db7
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-19 20:36:50 +0100

    #20 Implement fetch_a_specific_position

--- backend/src/core/services/brokers/Trading212Client.py
+++ backend/src/core/services/brokers/Trading212Client.py
@@ -4,7 +4,13 @@ from backend.src.core.services.brokers.AbstractBrokerClient import BrokerClient
 
 
 class Trading212(BrokerClient):
+    BASE_URL = "https://live.trading212.com"
 
     @staticmethod
     def validate_api_key(api_key: str, private_key: str) -> HTTPException | bool:
         return True
+
+    # Personal Portfolio
+    def fetch_a_specific_position(self, ticker: str) -> dict:
+        """Fetch an open position by ticker"""
+        return super().requests("GET", "/api/v0/equity/portfolio/" + ticker)
