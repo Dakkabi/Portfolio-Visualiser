9e698aa5317c85c8067a2623415628645f4dc0b5
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-19 21:58:41 +0100

    #19 Create handleAPIKeySubmit function with privateKey guard and text field values as parameters

--- frontend/src/components/settings/BrokerModal.tsx
+++ frontend/src/components/settings/BrokerModal.tsx
@@ -1,5 +1,5 @@
 import {getInstructionByName, isPrivateKeyRequired} from "./InstructionFactory.tsx";
-import React from "react";
+import React, {useState} from "react";
 
 interface BrokerModalProps {
     brokerName: string;
@@ -8,6 +8,22 @@ interface BrokerModalProps {
 }
 
 const BrokerModal: React.FC<BrokerModalProps> = ({ brokerName, isOpen, onClose }) => {
+    let [apiKey, setApiKey] = useState("");
+    let [privateKey, setPrivateKey] = useState("");
+
+    /**
+     * Handle verifying if the api key and optional private key are valid keys, then save to the database.
+     *
+     * @param brokerName The platform that provided the keys.
+     * @param apiKey The main key allowing access to the platform's routes.
+     * @param privateKey An optional private key.
+     */
+    function handleAPIKeySubmit(brokerName: string, apiKey: string, privateKey: string) {
+        if (!isPrivateKeyRequired(brokerName)) {privateKey = "";}
+
+
+    }
+
 
     if (!isOpen) return null;
 
@@ -23,16 +39,28 @@ const BrokerModal: React.FC<BrokerModalProps> = ({ brokerName, isOpen, onClose }
 
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend">Enter API Key</legend>
-                    <input type="text" className="input" placeholder="Not Hidden" />
+                    <input
+                        type="text"
+                        className="input"
+                        placeholder="Not Hidden"
+                        onChange={(newApiKey) => setApiKey(newApiKey.target.value)}
+                    />
 
                     {isPrivateKeyRequired(brokerName) ? (
                         <>
                             <legend className="fieldset-legend">Enter Private Key</legend>
-                            <input type="text" className="input" placeholder="Not Hidden" />
+                            <input
+                                type="text"
+                                className="input"
+                                placeholder="Not Hidden"
+                                onChange={
+                                (newPrivateKey) => setPrivateKey(newPrivateKey.target.value)
+                            }
+                            />
                         </>
                     ) : ""}
 
-                    <button type="submit" className="btn btn-wide mt-4">Submit</button>
+                    <button onClick={() => handleAPIKeySubmit(brokerName, apiKey, privateKey)} type="submit" className="btn btn-wide mt-4">Submit</button>
                 </fieldset>
             </div>
         </dialog>
