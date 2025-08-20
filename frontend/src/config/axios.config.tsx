3c4f694810faf76e51a891fdb4456af2ee16ebe4
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-20 16:17:15 +0100

    #19 Implement protectedAPI to automatically handle unauthorised responses and attach accessToken to requests

--- frontend/src/config/axios.config.tsx
+++ frontend/src/config/axios.config.tsx
@@ -3,13 +3,36 @@ import Axios from "axios";
 /**
  * General axios instance, for use with api endpoints that require no user authentication.
  */
-export const api = Axios.create({
+const api = Axios.create({
     baseURL: "/api",
 })
 
 /**
  * Protected axios instance, for use with api endpoints that require an access token.
  */
-export const protectedApi = Axios.create({
+const protectedApi = Axios.create({
     baseURL: "/api",
 })
+
+protectedApi.interceptors.request.use(
+    (config) => {
+        const accessToken = sessionStorage.getItem("accessToken");
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+)
+
+protectedApi.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            sessionStorage.removeItem("accessToken");
+            window.location.href = "/login";
+        }
+    }
+)
+
+export { api, protectedApi };
