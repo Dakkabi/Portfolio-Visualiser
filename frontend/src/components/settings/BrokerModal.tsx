9adccdc99aa3edbc47b594303f06c34c3b80e05b
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-21 21:07:02 +0100

    #19 Implement Adding an API key method with an Alert component for showing response

--- frontend/src/components/settings/BrokerModal.tsx
+++ frontend/src/components/settings/BrokerModal.tsx
@@ -1,5 +1,7 @@
 import getInstructionByBrokerName from "./InstructionFactory.tsx";
 import React, {useState} from "react";
+import {protectedApi} from "../../config/axios.config.tsx";
+import Alert from "../Alert.tsx";
 
 interface BrokerModalProps {
     brokerName: string;
@@ -12,6 +14,8 @@ const BrokerModal: React.FC<BrokerModalProps> = ({ brokerName, isOpen, onClose, 
     let [apiKey, setApiKey] = useState("");
     let [privateKey, setPrivateKey] = useState("");
 
+    let [alertProps, setAlertProps] = useState<{message: string, type: string}>({message: "", type: ""})
+
     /**
      * Handle verifying if the api key and private key are valid, then save to the database.
      *
@@ -19,9 +23,36 @@ const BrokerModal: React.FC<BrokerModalProps> = ({ brokerName, isOpen, onClose, 
      * @param apiKey The main key allowing access to the platform's routes.
      * @param privateKey A private key.
      */
-    function handleApiKeySubmit(brokerName: string, apiKey: string, privateKey: string) {
-        if (!isPrivateKeyRequired) {privateKey = "";}
+    async function handleApiKeySubmit(brokerName: string, apiKey: string, privateKey: string | null) {
+        if (!isPrivateKeyRequired) {privateKey = null;}
 
+        const endpoint = "/keys/";
+        const params = {
+            api_key: apiKey,
+            private_key: privateKey,
+            brokers_name: brokerName,
+        };
+
+        console.log(params);
+
+        try {
+            await protectedApi.post(endpoint, params);
+            setAlertProps({message: "Successfully added new API keys.", type: "alert-success"})
+
+        } catch (postError: any) {
+            if (postError.response && postError.response.status === 409) {
+                // API Key may already exist
+                try {
+                    await protectedApi.put(endpoint, params);
+                    setAlertProps({message: "Successfully updated API keys.", type: "alert-success"})
+
+                } catch (putError: any) {
+                    setAlertProps({message: `${putError.status}: ${putError.message}`, type: "alert-error"})
+                }
+            } else {
+                setAlertProps({message: `${postError.status}: ${postError.message}`, type: "alert-error"})
+            }
+        }
     }
 
     if (!isOpen) return null;
@@ -34,6 +65,11 @@ const BrokerModal: React.FC<BrokerModalProps> = ({ brokerName, isOpen, onClose, 
                 {getInstructionByBrokerName(brokerName)}
 
                 <div className="divider"></div>
+
+                <Alert
+                    message={alertProps.message}
+                    type={alertProps.type}
+                />
 
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend">Enter API Key</legend>
