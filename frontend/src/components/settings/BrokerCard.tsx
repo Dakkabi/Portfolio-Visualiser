9a31351f45ecacb7ddb2229190e30b193a7c0aa5
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-21 22:01:30 +0100

    #19 Display green status bubble on BrokerCard only if showStatus prop is true

--- frontend/src/components/settings/BrokerCard.tsx
+++ frontend/src/components/settings/BrokerCard.tsx
@@ -3,13 +3,19 @@ import React from "react";
 interface BrokerCardProps {
     brokerName: string;
     cardClickEvent: (brokerName: string) => void;
+    showStatus: boolean;
 }
 
-const BrokerCard: React.FC<BrokerCardProps> = ({ brokerName, cardClickEvent }) => {
+const BrokerCard: React.FC<BrokerCardProps> = ({ brokerName, cardClickEvent, showStatus }) => {
 
     return (
         <div>
             <a onClick={() => cardClickEvent(brokerName)} className="card w-87 cursor-pointer">
+                {showStatus ?
+                    <div aria-label="status" className="status status-xl status-success absolute top-2 right-2" />
+                    :
+                    ""
+                }
                 <figure>
                     <img
                         src={`/Brokers/${brokerName}/${brokerName}Billboard.webp`}
