dc89b1ccd59d33834bf75374b90fe95c418402cc
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-26 22:59:04 +0100

    #25 Add brokers relationship, add id and last_updated attributes

--- backend/src/database/models/portfolio_model.py
+++ backend/src/database/models/portfolio_model.py
@@ -1,4 +1,6 @@
-from sqlalchemy import JSON, ForeignKey
+from datetime import datetime, timezone
+
+from sqlalchemy import JSON, ForeignKey, Integer, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.src.database.session import Base
@@ -7,7 +9,12 @@ from backend.src.database.session import Base
 class Portfolio(Base):
     __tablename__ = "portfolios"
 
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     portfolio: Mapped[dict] = mapped_column(JSON)
+    last_updated: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
-    users_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    users_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     users: Mapped["User"] = relationship(back_populates="portfolios")
+
+    brokers_name: Mapped[str] = mapped_column(ForeignKey("brokers.name"), nullable=True, unique=True)
+    brokers: Mapped["Broker"] = relationship(back_populates="portfolios")
