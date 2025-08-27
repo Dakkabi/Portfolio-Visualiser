0dea2363d3bc28e2e940cd40cdf5e83e0ee96c6b
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-08-27 20:40:48 +0100

    #25 Update CRUD implementations to use new pydantic schemas

--- backend/src/database/crud/portfolio_crud.py
+++ backend/src/database/crud/portfolio_crud.py
@@ -4,24 +4,25 @@ from backend.src.database.models.portfolio_model import Portfolio
 from backend.src.schemas.models.portfolio_schema import PortfolioCreate, PortfolioUpdate
 
 
-def get_db_portfolio_by_user_id(db: Session, user_id: int):
-    return db.query(Portfolio).filter_by(user_id=user_id).first()
+def get_db_portfolio_by_user_id_and_broker_name(db: Session, user_id: int, broker_name: str):
+    return db.query(Portfolio).filter_by(user_id=user_id, brokers_name=broker_name).first()
 
-def create_db_portfolio(db: Session, data: PortfolioCreate, user_id: int) -> Portfolio:
+def create_db_portfolio(db: Session, portfolio: PortfolioCreate, user_id: int) -> Portfolio:
     """Create a new portfolio entry in the database."""
     db_portfolio = Portfolio(
         users_id=user_id,
-        portfolio=data.model_dump()
+        portfolio={**portfolio.portfolio},
+        brokers_name=portfolio.broker_name,
     )
     db.add(db_portfolio)
     db.commit()
     db.refresh(db_portfolio)
     return db_portfolio
 
-def update_db_portfolio(db: Session, data: PortfolioUpdate, user_id: int):
+def update_db_portfolio(db: Session, portfolio: PortfolioUpdate, user_id: int):
     """Update an existing portfolio entry in the database."""
-    db_portfolio = get_db_portfolio_by_user_id(db, user_id)
-    db_portfolio.portfolio = data.model_dump()
+    db_portfolio = get_db_portfolio_by_user_id_and_broker_name (db, user_id, portfolio.broker_name)
+    db_portfolio.portfolio = {**portfolio.portfolio}
     db.commit()
     db.refresh(db_portfolio)
     return db_portfolio
