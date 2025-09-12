aec65d2ee5fc8084d8a91730c7972bc91432be62
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-09-12 19:14:22 +0100

    #27 Add variable for broker_rate_limit

--- backend/src/core/services/models/portfolio_service.py
+++ backend/src/core/services/models/portfolio_service.py
@@ -20,13 +20,16 @@ def create_or_update_portfolio(
     :param broker_name: The name of the broker
     :param user_id: The id of the user
     """
-    if get_db_broker(db, broker_name) is None:
+    broker = get_db_broker(db, broker_name)
+    if broker is None:
         raise HTTPException(status_code=404, detail=f"Broker '{broker_name}' not found")
 
     if (db_api_keys := get_db_api_key(db, user_id, broker_name)) is None:
         raise HTTPException(status_code=409, detail="User does not have API keys for the broker requested")
 
     current_portfolio = get_db_portfolio_by_user_id_and_broker_name(db, user_id, broker_name)
+    broker_rate_limit = broker.rate_limit
+
     new_portfolio = build_portfolio(broker_name, db_api_keys.api_key, db_api_keys.private_key).to_dict()
 
     if current_portfolio is None:
