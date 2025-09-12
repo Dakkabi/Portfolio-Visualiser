002a1e7ac3c165a8fbb7d97e6bb95bf86f01cc92
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-09-12 15:51:35 +0100

    #27 Replace last_updated attribute in portfolios table to epoch int rather than datetime

--- backend/src/database/models/portfolio_model.py
+++ backend/src/database/models/portfolio_model.py
@@ -1,8 +1,7 @@
-from datetime import datetime, timezone
-
-from sqlalchemy import JSON, ForeignKey, Integer, DateTime
+from sqlalchemy import JSON, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from backend.src.core.services.time_service import epoch_now
 from backend.src.database.session import Base
 
 
@@ -11,7 +10,7 @@ class Portfolio(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     portfolio: Mapped[dict] = mapped_column(JSON)
-    last_updated: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+    last_updated: Mapped[int] = mapped_column(Integer, default=epoch_now(), onupdate=epoch_now())
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     users: Mapped["User"] = relationship(back_populates="portfolios")
