f67b2c3b1b32fe41eae92753eded35fe6e0c1a39
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-09-12 16:14:28 +0100

    #27 Implement portfolio CRUD method for getting all portfolios by user_id

--- backend/src/database/crud/portfolio_crud.py
+++ backend/src/database/crud/portfolio_crud.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from backend.src.database.models.portfolio_model import Portfolio
 from backend.src.schemas.models.portfolio_schema import PortfolioCreate, PortfolioUpdate
 
+def get_db_portfolio_by_user_id(db: Session, user_id: int):
+    return db.query(Portfolio).filter_by(user_id=user_id).all()
 
 def get_db_portfolio_by_user_id_and_broker_name(db: Session, user_id: int, broker_name: str):
     return db.query(Portfolio).filter_by(user_id=user_id, broker_name=broker_name).first()
