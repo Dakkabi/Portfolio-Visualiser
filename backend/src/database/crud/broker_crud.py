feeac5ecb2b5d499865b8942bee62b3906e222a4
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-09-12 15:31:25 +0100

    #27 Refactor "read_file_into_broker_table" to handle json file types

--- backend/src/database/crud/broker_crud.py
+++ backend/src/database/crud/broker_crud.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 
 from sqlalchemy.orm import Session
@@ -27,6 +28,7 @@ def create_db_broker(db: Session, broker: BrokerCreate) -> Broker:
         name=broker.name,
         type=broker.type,
         private_key_required=broker.private_key_required,
+        rate_limit=broker.rate_limit,
     )
     db.add(db_broker)
     db.commit()
@@ -34,46 +36,26 @@ def create_db_broker(db: Session, broker: BrokerCreate) -> Broker:
     return db_broker
 
 def read_file_into_broker_table(db: Session, file_path: str = None):
-    """Read a file, line by line, and add it as a record to the brokers table.
+    """Read a json file into the broker's table.
 
     :param db: The database session.
-    :param file_path: The path to the file to read, default is `/backend/src/assets/supported_brokers.json`.
+    :param file_path: The path to the file to read, default to SUPPORTED_BROKERS_PATH.
+    :return: None
     """
-    def convert_string_to_bool(string: str) -> bool:
-        """Convert a string to boolean value."""
-        bool_map = {"true": True, "false": False}
-        try:
-            return bool_map[string.strip().lower()]
-        except KeyError:
-            raise ValueError(f"Couldn't convert string value '{string}' to boolean value.")
-
     if file_path is None:
         file_path = SUPPORTED_BROKERS_PATH
 
-    with open(file_path, "r") as file:
-        for line in file:
-            line = line.strip()
+    with open(file_path, "r") as f:
+        brokers = json.load(f)
 
-            line_split = line.split(":")
-            if len(line_split) != 3: raise ValueError(f"Invalid line format: {line}")
+    for broker_name, broker_info in brokers.items():
+        if get_db_broker(db, broker_name) is None:
+            new_broker = BrokerCreate(
+                name=broker_name,
+                type=broker_info["asset_types"],
+                private_key_required=broker_info["private_key_required"],
+                rate_limit=broker_info["rate_limit"],
+            )
 
-            broker_name = line_split[0]
-            private_key_required = line_split[1]
-            asset_type_list = line_split[2]
-
-            asset_types = asset_type_list.split(",")
-
-            if broker_name != "NAME": # Header line
-                if get_db_broker(db, broker_name) is None:
-                    asset_type_enum_list = []
-                    for asset_type in asset_types:
-                        asset_type_enum_list.append(AssetType(asset_type))
-
-                    broker_create = BrokerCreate(
-                        name=broker_name,
-                        type=asset_type_enum_list,
-                        private_key_required=convert_string_to_bool(private_key_required),
-                    )
-
-                    create_db_broker(db, broker_create)
+            create_db_broker(db, new_broker)
 
