6eb023c7526f2fe615fe1e36d8d5f2c7bded0611
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-09-15 15:35:12 +0100

    #31 Add 4 stat components for Total Portfolio Value, Daily Change, Unrealised Gain/Loss and Total Dividend Earning YTD

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,11 +5,28 @@ function Dashboard() {
         <div>
             <GlobalNavbar currentPage="Dashboard" />
             <div className="min-h-screen bg-base-200">
-                <div className="stats shadow bg-base-100">
-                    <div className="stat">
-                        <div className="stat-figure text-secondary"></div>
-                        <div className="stat-title">Total Portfolio Value</div>
-                        <div className="stat-value">$120,000</div>
+                <div className="p-4">
+                    <span className="font-bold text-2xl p-4">Total Dashboard</span>
+                </div>
+
+                <div className="ms-10 mt-5 mr-10">
+                    <div className="stats shadow bg-base-100 w-full">
+                        <div className="stat">
+                            <div className="stat-title">Total Portfolio Value</div>
+                            <div className="stat-value">£120,000.00</div>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">Day Change</div>
+                            <div className="stat-value text-success">+£2300.00 (1.95%)</div>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">Unrealised Gain/Loss</div>
+                            <div className="stat-value text-error">-£20,000.00 (-14.29%)</div>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">Total Dividend Earnings Year To Date</div>
+                            <div className="stat-value text-info">£728.24</div>
+                        </div>
                     </div>
                 </div>
             </div>
