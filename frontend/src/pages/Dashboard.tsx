8dc804aca974dded9a55d8569946ec16c882948b
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-09-15 15:46:11 +0100

    #31 Make stat text easier to read

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -12,19 +12,19 @@ function Dashboard() {
                 <div className="ms-10 mt-5 mr-10">
                     <div className="stats shadow bg-base-100 w-full">
                         <div className="stat">
-                            <div className="stat-title">Total Portfolio Value</div>
+                            <div className="stat-title text-black">Total Portfolio Value</div>
                             <div className="stat-value">£120,000.00</div>
                         </div>
                         <div className="stat">
-                            <div className="stat-title">Day Change</div>
+                            <div className="stat-title text-black">Day Change</div>
                             <div className="stat-value text-success">+£2300.00 (1.95%)</div>
                         </div>
                         <div className="stat">
-                            <div className="stat-title">Unrealised Gain/Loss</div>
+                            <div className="stat-title text-black">Unrealised Gain/Loss</div>
                             <div className="stat-value text-error">-£20,000.00 (-14.29%)</div>
                         </div>
                         <div className="stat">
-                            <div className="stat-title">Total Dividend Earnings Year To Date</div>
+                            <div className="stat-title text-black">Total Dividend Earnings Year To Date</div>
                             <div className="stat-value text-info">£728.24</div>
                         </div>
                     </div>
