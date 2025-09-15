558ae39607d1b911c8b413e8f3ed2d5a368ce2a0
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-09-15 14:17:43 +0100

    #31 Mockup with stat component for Dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,7 +2,18 @@ import GlobalNavbar from "../components/global/GlobalNavbar.tsx";
 
 function Dashboard() {
     return (
-        <GlobalNavbar currentPage="Dashboard" />
+        <div>
+            <GlobalNavbar currentPage="Dashboard" />
+            <div className="min-h-screen bg-base-200">
+                <div className="stats shadow bg-base-100">
+                    <div className="stat">
+                        <div className="stat-figure text-secondary"></div>
+                        <div className="stat-title">Total Portfolio Value</div>
+                        <div className="stat-value">$120,000</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
