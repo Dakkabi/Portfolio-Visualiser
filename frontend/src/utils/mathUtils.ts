6337e4ef9b13b728e573133441856c6bb68fd829
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-09-16 22:28:50 +0100

    #31 Create mathUtil file with percentageChange method implementation

---
+++ frontend/src/utils/mathUtils.ts
@@ -0,0 +1,11 @@
+/**
+ * Return the percentage change difference between two values.
+ *
+ * @param old_value - The old value.
+ * @param new_value - The new value.
+ * @return number - The percentage change as a percentage to one degree accuracy.
+ */
+export function percentageChange(old_value: number, new_value: number) {
+    const difference = new_value - old_value
+    return (difference / old_value) * 100
+}
