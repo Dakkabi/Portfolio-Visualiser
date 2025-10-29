4229a53122850649492046c5baede0ef88a37ee5
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-10-29 15:50:13 +0000

    #1 Create .env config loader to give type casting to .env variables

---
+++ backend/src/core/config.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from pydantic import computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import psycopg2
+
+load_dotenv() # Load .env into memory
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra="ignore",
+        env_ignore_empty=True
+    )
+
+    POSTGRESQL_USERNAME: str
+    POSTGRESQL_PASSWORD: str
+    POSTGRESQL_SERVER: str
+    POSTGRESQL_PORT: int
+    POSTGRESQL_DATABASE: str
+
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return "{}://{}:{}@{}:{}/{}".format(
+            "postgresql+psycopg2",
+            self.POSTGRESQL_USERNAME,
+            self.POSTGRESQL_PASSWORD,
+            self.POSTGRESQL_SERVER,
+            self.POSTGRESQL_PORT,
+            self.POSTGRESQL_DATABASE
+        )
