083e409207c6241d319f3647842f31e98bed267c
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-10-29 19:04:18 +0000

    #3 Hook auth_router and user_router onto FastAPI main app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 
+from backend.src.routes.models.user_router import user_router
 from backend.src.routes.security.authentication_router import auth_router
 
 app = FastAPI()
 
 ROUTER_PREFIX = "/api"
 app.include_router(auth_router, prefix=ROUTER_PREFIX)
+app.include_router(user_router, prefix=ROUTER_PREFIX)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
