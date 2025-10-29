a4f25e97ba0eb0e5c6556bacb939248e773fd2b2
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-10-29 18:58:00 +0000

    #3 Implement creating and fetching a user by email CRUD operations

---
+++ backend/src/database/crud/user_crud.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+
+from backend.src.database.models.user_model import User
+from backend.src.schemas.models.user_schema import UserCreate
+from backend.src.services.security.cryptography import get_password_hash
+
+
+def get_db_user_by_email(db: Session, email: str) -> type[User] | None:
+    return db.query(User).filter(User.email == email).first()
+
+def create_db_user(db: Session, new_user: UserCreate) -> User:
+    db_user = User(
+        email=new_user.email,
+        password=get_password_hash(new_user.password),
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
