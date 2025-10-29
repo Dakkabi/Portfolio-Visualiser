f2b83030146d6f626143b97e08a3859e3231914e
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-10-29 18:57:16 +0000

    #3 Add starlette and bcrypt to requirements

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
+from backend.src.routes.security.authentication_router import auth_router
+
 app = FastAPI()
+
+ROUTER_PREFIX = "/api"
+app.include_router(auth_router, prefix=ROUTER_PREFIX)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
