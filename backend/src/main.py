5a0a9b12885d3886b01c031d2932eb31cc40e272
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-11-04 22:07:39 +0000

    #5 Hook api key router to main fastapi app

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 
 from backend.src.database.crud.broker_crud import create_db_broker_from_file
 from backend.src.database.session import get_db
+from backend.src.routes.models.api_key_router import api_key_router
 from backend.src.routes.models.user_router import user_router
 from backend.src.routes.security.authentication_router import auth_router
 
@@ -25,6 +26,7 @@ app = FastAPI(lifespan=lifespan)
 ROUTER_PREFIX = "/api"
 app.include_router(auth_router, prefix=ROUTER_PREFIX)
 app.include_router(user_router, prefix=ROUTER_PREFIX)
+app.include_router(api_key_router, prefix=ROUTER_PREFIX)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
