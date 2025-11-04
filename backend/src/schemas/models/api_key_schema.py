8e34b29563549cc0412113ad2192530627a3bd4c
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-11-04 22:08:18 +0000

    #5 Create ApiKey schema with ApiKeyCreate

---
+++ backend/src/schemas/models/api_key_schema.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class ApiKeyBase(BaseModel):
+    api_key: str
+    secret_key: Optional[str] = None
+    broker_name: str
+
+class ApiKeyCreate(ApiKeyBase):
+    pass
+
+class ApiKeySchema(ApiKeyBase):
+    pass
