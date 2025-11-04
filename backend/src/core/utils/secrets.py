28e0012735771c26780117041b7ccc4d9d346b01
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-11-04 22:18:09 +0000

    #5 Create convert_to_str and convert_to_bytes helper functions that use base64 encoding/decoding

--- backend/src/core/utils/secrets.py
+++ backend/src/core/utils/secrets.py
@@ -18,24 +18,26 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def generate_symmetric_key() -> str:
     return base64.b64encode(ChaCha20Poly1305.generate_key()).decode("utf-8")
 
-def get_key_in_bytes(key: str) -> bytes:
-    return base64.b64decode(key.encode("utf-8"))
+def decode_to_bytes(data: str) -> bytes:
+    return base64.b64decode(data.encode("utf-8"))
 
-def generate_nonce(size: int = 16) -> bytes:
+def encode_to_str(data: bytes) -> str:
+    return base64.b64encode(data).decode("utf-8")
+
+def generate_nonce(size: int = 12) -> bytes:
     return os.urandom(size)
 
-AES_SECRET_KEY = get_key_in_bytes(settings.AES_SECRET_KEY)
+AES_SECRET_KEY = decode_to_bytes(settings.AES_SECRET_KEY)
 CHACHA = ChaCha20Poly1305(AES_SECRET_KEY)
 
 def encrypt_api_key(api_key: str) -> str:
     nonce = generate_nonce()
     ciphertext = CHACHA.encrypt(nonce, api_key.encode("utf-8"), b"")
-    return f"{base64.b64encode(ciphertext).decode("utf-8")}::{nonce.decode("utf-8")}"
+    return f"{encode_to_str(ciphertext)}::{encode_to_str(nonce)}"
 
 def decrypt_api_key(encrypted_api_key: str) -> str:
     ciphertext, nonce = encrypted_api_key.split("::")
-    ciphertext = base64.b64decode(ciphertext.encode("utf-8"))
-    return CHACHA.decrypt(nonce.encode("utf-8"), ciphertext, b"").decode("utf-8")
+    return CHACHA.decrypt(decode_to_bytes(nonce), decode_to_bytes(nonce), b"").decode("utf-8")
 
 if __name__ == "__main__":
     print(generate_symmetric_key())
