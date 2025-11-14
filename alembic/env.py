260ba4fea0ab684453bed44d25409569e5ea036f
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-11-14 00:06:26 +0000

    #11 Hook portfolios_cash table to alembic

--- alembic/env.py
+++ alembic/env.py
@@ -10,6 +10,7 @@ from backend.src.database.session import Base
 from backend.src.database.models.user_model import User
 from backend.src.database.models.broker_model import Broker
 from backend.src.database.models.api_key_model import ApiKeys
+from backend.src.database.models.portfolio_model import PortfolioCash
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
