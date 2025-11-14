4434d0ce8a1e38ad7f691e910a20c37bf65668d9
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-11-14 00:09:21 +0000

    #11 Hook portfolio_router to FastAPI app entrypoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from backend.src.database.crud.broker_crud import create_db_broker_from_file
 from backend.src.database.session import get_db
 from backend.src.routes.models.api_key_router import api_key_router
+from backend.src.routes.models.portfolio_router import portfolio_router
 from backend.src.routes.models.user_router import user_router
 from backend.src.routes.security.authentication_router import auth_router
 
@@ -27,6 +28,7 @@ ROUTER_PREFIX = "/api"
 app.include_router(auth_router, prefix=ROUTER_PREFIX)
 app.include_router(user_router, prefix=ROUTER_PREFIX)
 app.include_router(api_key_router, prefix=ROUTER_PREFIX)
+app.include_router(portfolio_router, prefix=ROUTER_PREFIX)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
