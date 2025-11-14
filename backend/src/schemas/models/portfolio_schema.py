008e4638a8cdd4b81e6def893f8020e99571a0da
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-11-14 00:12:19 +0000

    #11 Create portfolio schema for response_model and create schema

---
+++ backend/src/schemas/models/portfolio_schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class PortfolioCashBase(BaseModel):
+    free: float
+    invested: float
+    profit_and_loss: float
+    result: float
+    total: float
+    broker_name: str
+
+class PortfolioCashCreate(PortfolioCashBase):
+    pass
+
+class PortfolioCashSchema(PortfolioCashBase):
+    pass
