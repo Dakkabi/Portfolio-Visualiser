9220508121ec4927643058b84c08785146accfd4
Dominic <240941065+Dakkabi@users.noreply.github.com>
2025-11-14 22:37:14 +0000

    #11 Update get_db_broker_by_name to get_db_broker as it searches by primary key

--- backend/src/database/crud/broker_crud.py
+++ backend/src/database/crud/broker_crud.py
@@ -2,11 +2,11 @@ import os
 
 from sqlalchemy.orm import Session
 
-from backend.src.core.utils.path_util import get_resources_path
+from backend.src.services.utils.path_util import get_resources_path
 from backend.src.database.models.broker_model import Broker
 from backend.src.schemas.models.broker_schema import BrokerCreate
 
-def get_db_broker_by_name(db: Session, name: str):
+def get_db_broker(db: Session, name: str):
     return db.query(Broker).filter(Broker.name == name).first()
 
 def create_db_broker(db: Session, broker: BrokerCreate):
@@ -29,7 +29,7 @@ def create_db_broker_from_file(db: Session, file_path: str = None):
 
     with open(file_path, "r") as f:
         for line in f:
-            if get_db_broker_by_name(db=db, name=line.strip()):
+            if get_db_broker(db=db, name=line.strip()):
                 continue
 
             create_db_broker(db, BrokerCreate(name=line.strip()))
